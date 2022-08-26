88fc9f788d8b56f7d07605827e1380008d0ab298
Vladimir Mitrev <orionsbelt1017@gmail.com>
2022-08-26 17:19:36 +0300

    Basic Web Project - Phone book

---
+++ BasicWebProject/phonebook/src/main/java/phonebook/controller/ContactController.java
@@ -0,0 +1,42 @@
+package phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.ModelAndView;
+import phonebook.entity.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ContactController {
+    private List<Contact> contacts;
+    public ContactController() {
+        this.contacts = new ArrayList<>();
+    }
+@GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        modelAndView.addObject("contacts", this.contacts);
+        return modelAndView;
+    }
+@PostMapping("/")
+    public String add(Contact contact) {
+        this.contacts.add(contact);
+
+        return "redirect:/";
+    }
+    @PutMapping("/")
+    public String update(Contact updatedContact) {
+        for (Contact contact : contacts) {
+            if(contact.getName().equals(updatedContact.getName())) {
+                this.contacts.remove(contact);
+                this.contacts.add(updatedContact);
+            }
+
+        }
+        return "redirect:/";
+    }
+}
