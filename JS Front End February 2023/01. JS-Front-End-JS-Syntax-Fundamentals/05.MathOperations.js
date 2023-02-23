54debd569898c7ba7d4ebe4c41e169e1bf007a7c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 09:52:56 +0200

    05.MathOperations

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/05.MathOperations.js
@@ -0,0 +1,36 @@
+function solve(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    case "%":
+      result = num1 % num2;
+      break;
+    case "**":
+      result = num1 ** num2;
+      break;
+    default:
+      console.log("Error!");
+  }
+  console.log(result);
+}
+solve(5, 6, "+");
+solve(3, 5.5, "*");
+solve(6, 4, "+");
+solve(6, 4, "-");
+solve(6, 4, "*");
+solve(6, 4, "/");
+solve(6, 4, "%");
+solve(6, 4, "**");
+solve(3, 5.5, "s");
