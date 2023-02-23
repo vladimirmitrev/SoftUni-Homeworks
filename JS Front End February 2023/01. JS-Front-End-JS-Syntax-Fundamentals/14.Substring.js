7f851aa06e4c91e95c1dcfeaa1583791eed4fe8e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 12:13:26 +0200

    14.Substring.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/14.Substring.js
@@ -0,0 +1,12 @@
+function substring(word, startIndex, countOfElements) {
+    
+    let result;
+
+    result = word.substr(startIndex, countOfElements);
+
+
+    console.log(result);
+}
+
+substring('ASentence', 1, 8);
+substring('SkipWord', 4, 7);
