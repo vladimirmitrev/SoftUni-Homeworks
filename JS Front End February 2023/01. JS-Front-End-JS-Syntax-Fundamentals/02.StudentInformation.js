8058dbc46ca2c750e5e7a37a52aaed7a71ac5cf3
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 09:35:01 +0200

    02.StudentInformation

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/02.StudentInformation.js
@@ -0,0 +1,7 @@
+function solve (name,age,grade) {
+   console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+solve('John', 15, 5.54678);
+solve('Ivan', 20, 3.34756);
+solve('George', 33, 5.94783);
