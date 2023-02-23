9adbfee2d4e67e19418ff22ec7f570c576a72a7f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 11:53:07 +0200

    12.ReverseAnArrayOfNumbers added reverse method

--- JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/12.ReverseAnArrayOfNumbers.js
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/12.ReverseAnArrayOfNumbers.js
@@ -1,17 +1,20 @@
 function reverse(n, inputArr) {
-    let array = [];
-    for (let i = 0; i < n; i++) {
-        let number = inputArr[i];
-        array.push(number);
-    }
+  let array = [];
+  for (let i = 0; i < n; i++) {
+    let number = inputArr[i];
+    array.push(number);
+  }
 
-    let output = '';
-    for (let i = array.length -1; i >= 0 ; i--) {
-        output += array[i] += ' ';
-    }
-    output.trimEnd;
-    console.log(output);
+  let reversedArray = array.reverse();
+  console.log(reversedArray.join(" "));
 
+  // let output = '';
+  // for (let i = array.length -1; i >= 0 ; i--) {
+  //     output += array[i] += ' ';
+  // }
+
+  // output.trimEnd;
+  // console.log(output);
 }
 
 reverse(3, [10, 20, 30, 40, 50]);
