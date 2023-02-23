450800b0af7400fdd8311a85ba2d820f3b38af5d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 12:04:47 +0200

    13.EvenAndOddSubtractios

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/13.EvenAndOddSubtraction.js
@@ -0,0 +1,17 @@
+function calculate(inputArr) {
+  let evenSum = 0;
+  let oddSum = 0;
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] % 2 === 0) {
+      evenSum += inputArr[i];
+    } else {
+      oddSum += inputArr[i];
+    }
+  }
+  let result = evenSum - oddSum;
+  console.log(result);
+}
+
+calculate([1,2,3,4,5,6]);
+calculate([3,5,7,9]);
+calculate([2,4,6,8,10]);
