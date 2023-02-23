d681f0aa637acd4fbdbcb4b1e48d71ae2799c00e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-23 10:56:42 +0200

    11.SumFirstAndLastArrayElements.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/11.SumFirstAndLastArrayElements.js
@@ -0,0 +1,13 @@
+function solve(input) {
+  let firstNum = input[0];
+
+  let lastNum = input[input.length - 1];
+
+  let result = firstNum + lastNum;
+
+  console.log(result);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
