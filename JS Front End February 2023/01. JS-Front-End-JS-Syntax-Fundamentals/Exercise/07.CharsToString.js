0ca5efc24027d2b368d5aae2230d8813382199a0
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-24 20:03:01 +0200

    Exercise/07.CharsToString.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/07.CharsToString.js
@@ -0,0 +1,10 @@
+function charsToString(firstChar, secondChar, thirdChar) {
+  let word = "";
+  word = firstChar + secondChar + thirdChar;
+
+  console.log(word);
+}
+
+charsToString("a", "b", "c");
+charsToString("%", "2", "o");
+charsToString("1", "5", "p");
