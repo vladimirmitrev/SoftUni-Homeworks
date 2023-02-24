d1c358880232972a3a531623482798d656741f21
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-24 08:40:03 +0200

    Lab

--- JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Lab/13.EvenAndOddSubtraction.js
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Lab/13.EvenAndOddSubtraction.js
@@ -1,7 +1,9 @@
 function calculate(inputArr) {
   let evenSum = 0;
   let oddSum = 0;
+
   for (let i = 0; i < inputArr.length; i++) {
+
     if (inputArr[i] % 2 === 0) {
       evenSum += inputArr[i];
     } else {
