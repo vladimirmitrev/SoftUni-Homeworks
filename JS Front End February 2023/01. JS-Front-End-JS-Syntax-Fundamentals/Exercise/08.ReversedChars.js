b185372a3c229663a2b18a7a41891dfb3a334d64
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-24 20:07:24 +0200

    Exercise/08.ReversedChars.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/08.ReversedChars.js
@@ -0,0 +1,9 @@
+function reversedChars(firstChar, secondChar, thirdChar) {
+  let result = "";
+  result = thirdChar + " " + secondChar + " " + firstChar;
+
+  console.log(result);
+}
+
+reversedChars("A", "B", "C");
+reversedChars("1", "L", "&");
