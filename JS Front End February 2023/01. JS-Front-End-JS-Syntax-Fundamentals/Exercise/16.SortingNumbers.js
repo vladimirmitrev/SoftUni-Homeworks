95f868a89f6e0ec012783aecf9d65b0784a32d09
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-25 12:46:43 +0200

    Exercise/16.SortingNumbers.js

--- JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/16.SortingNumbers.js
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/16.SortingNumbers.js
@@ -1,13 +1,11 @@
 function sortingNumbers(numbers) {
-    
-    let sortedNumbers = [...numbers].sort((aNum, bNum) => 
-    aNum - bNum);
-    let result = [];
-    while (sortedNumbers.length > 0) {
-        result.push(sortedNumbers.shift());
-        result.push(sortedNumbers.pop());
-    }
-    return result;
+  let sortedNumbers = [...numbers].sort((aNum, bNum) => aNum - bNum);
+  let result = [];
+  while (sortedNumbers.length > 0) {
+    result.push(sortedNumbers.shift());
+    result.push(sortedNumbers.pop());
+  }
+  return result;
 }
 
 console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
