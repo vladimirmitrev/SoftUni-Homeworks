8b3ba3e72396ba1dad81360b50f18e7544a092c4
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-25 11:09:49 +0200

    13.ArrayRotation.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/13.ArrayRotation.js
@@ -0,0 +1,15 @@
+function arrayRotation(numberArr, countOfRotations) {
+
+    countOfRotations %= numberArr.length;
+
+    for (let i = 1; i <= countOfRotations; i++) {
+        let firstNum = numberArr.shift();
+        numberArr.push(firstNum);
+    }
+    console.log(numberArr.join(" "));
+
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
+arrayRotation([32, 21, 61, 1], 4);
+arrayRotation([2, 4, 15, 31], 5);
