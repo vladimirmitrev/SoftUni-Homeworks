9e0fc34067d6c8460f849de836f4a627d5f89b8c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-25 14:04:09 +0200

    Exercise/18.ModernTimesOf#.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/Exercise/18.ModernTimesOf#.js
@@ -0,0 +1,33 @@
+function modernTimes(text) {
+  //   * Solution One with REGEX* //
+
+  let textArr = text.split(" ");
+  let arrToPrint = [];
+  for (const word of textArr) {
+    if (/^#[a-zA-Z]+$/.test(word)) {
+      arrToPrint.push(word.slice(1));
+    }
+  }
+  console.log(arrToPrint.join("\n"));
+}
+modernTimes("Nowadays everyone uses # to tag a #special word in #socialMedia");
+modernTimes(
+  "The symbol # is known #variously in English-speaking #regions as the #number sign"
+);
+// * Solution Two * //
+
+//   return text.split(" ")
+//             .filter((word) => word.startsWith("#") && containsOnlyLetters(word))
+//             .map((word) => word.slice(1))
+//             .filter((word) => word != "")
+//             .join("\n");
+// }
+
+// function containsOnlyLetters(word) {
+//     return [...word.toLowerCase()]
+//             .slice(1)
+//             .map((symbol) => symbol.charCodeAt(0))
+//             .every((charCode) => charCode >= 97 && charCode <= 122);
+// }
+
+// console.log(modernTimes("Nowadays everyone uses # to tag a #special word in #socialMedia"));
