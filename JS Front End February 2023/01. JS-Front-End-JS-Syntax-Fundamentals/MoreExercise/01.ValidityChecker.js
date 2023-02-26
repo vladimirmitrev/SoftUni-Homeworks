2bc07ba6658161b66714dfd8668b20e30d1cab71
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-02-26 09:58:33 +0200

    MoreExercise/01.ValidityChecker.js

---
+++ JS Front End February 2023/01. JS-Front-End-JS-Syntax-Fundamentals/MoreExercise/01.ValidityChecker.js
@@ -0,0 +1,26 @@
+function validityChecker(x1, y1, x2, y2) {
+  let firstDistance = Math.sqrt(x1 ** 2 + y1 ** 2);
+  let secondDistance = Math.sqrt(x2 ** 2 + y2 ** 2);
+  let thirdDistance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+  if (Number.isInteger(firstDistance)) {
+    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+  }
+
+  if (Number.isInteger(secondDistance)) {
+    console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+  }
+
+  if (Number.isInteger(thirdDistance)) {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+  }
+}
+
+validityChecker(3, 0, 0, 4);
+validityChecker(2, 1, 1, 1);
