6944c7cd62bc13c5769fed71225d766796ee1d1c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-02 13:13:39 +0200

    02.JS-Front-End-Functions-and-Statements

---
+++ JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Lab/01.FormatGrade.js
@@ -0,0 +1,21 @@
+function formatGrade(grade) {
+
+  if (grade >= 5.5) {
+    return `Excellent (${gradeToFixed(grade)})`;
+  } else if (grade < 5.5 && grade >= 4.5) {
+    return `Very good (${gradeToFixed(grade)})`;
+  } else if (grade < 4.5 && grade >= 3.5) {
+    return `Good (${gradeToFixed(grade)})`;
+  } else if (grade < 3.5 && grade >= 3) {
+    return `Poor (${gradeToFixed(grade)})`;
+  }
+  return `Fail (2)`;
+
+  function gradeToFixed(grade) {
+     return grade.toFixed(2);
+  }
+}
+
+console.log(formatGrade(3.33));
+console.log(formatGrade(4.50));
+console.log(formatGrade(2.99));
