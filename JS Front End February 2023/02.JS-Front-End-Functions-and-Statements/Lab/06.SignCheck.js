2d8820455dcfc594fc020bdf3bc4ad15bfae9e03
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-03 09:29:36 +0200

    Lab/06.SignCheck.js

---
+++ JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Lab/06.SignCheck.js
@@ -0,0 +1,9 @@
+function signCheck(...numbers) {
+  return numbers.filter((num) => num < 0).length % 2 === 0
+    ? "Positive"
+    : "Negative";
+}
+
+console.log(signCheck(5, 12, -15));
+console.log(signCheck(-6, -12, 14));
+console.log(signCheck(-1, -2, -3));
