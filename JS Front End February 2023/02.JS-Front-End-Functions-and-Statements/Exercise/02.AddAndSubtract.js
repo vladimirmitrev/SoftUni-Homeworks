cf00b6d94dcf6c87234a4ea9d781f1a648a7f3f1
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-03 10:13:00 +0200

    Exercise/02.AddAndSubtract.js

--- JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Exercise/02.AddAndSubtract.js
+++ JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Exercise/02.AddAndSubtract.js
@@ -4,6 +4,14 @@ function calculations(firstNum, secondNum, thirdNum) {
 
   return subtract(sum(firstNum, secondNum), thirdNum);
 }
+//    *  Solution with arrow function *
+//
+// const calculations = (firstNum, secondNum, thirdNum) => {
+//   const sum = (a, b) => a + b;
+//   const subtract = (mySum, num) => mySum - num;
+
+//   return subtract(sum(firstNum, secondNum), thirdNum);
+// }
 
 console.log(calculations(23, 6, 10));
 console.log(calculations(1, 17, 30));
