e8696ff7c92ddac6ab85d4704389511713f608ea
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-03 09:53:53 +0200

    Exercise/01.SmallestOfThreeNumbers.js

--- JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Exercise/01.SmallestOfThreeNumbers.js
+++ JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Exercise/01.SmallestOfThreeNumbers.js
@@ -3,6 +3,9 @@ function findSmallestNumber(firstNum, secondNum, thirdNum) {
 
   return min;
 }
+
+// const findSmallestNumber = (firstNum, secondNum, thirdNum) =>  Math.min(firstNum, secondNum, thirdNum);
+
 console.log(findSmallestNumber(2, 5, 3));
 console.log(findSmallestNumber(600, 342, 123));
 console.log(findSmallestNumber(25, 21, 4));
