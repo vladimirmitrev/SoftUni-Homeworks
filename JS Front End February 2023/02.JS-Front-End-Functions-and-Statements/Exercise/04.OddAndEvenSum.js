29e8a35c2c5fcc24ea211cd5025da22dc25a82a5
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-03 20:14:07 +0200

    Exercise/04.OddAndEvenSum.js

---
+++ JS Front End February 2023/02.JS-Front-End-Functions-and-Statements/Exercise/04.OddAndEvenSum.js
@@ -0,0 +1,21 @@
+function oddAndEvenSum(number) {
+
+    let oddSum = 0;
+    let evenSum = 0;
+    const digits = number.toString().split("");
+
+
+    for (let i = 0; i < digits.length; i++) {
+        const digit = Number(digits[i]);
+        if(digit % 2 == 0) {
+            evenSum += digit;
+        } else {
+            oddSum += digit;
+        }
+    }
+
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`)
+}
+
+oddAndEvenSum(1000435);
+oddAndEvenSum(3495892137259234);
