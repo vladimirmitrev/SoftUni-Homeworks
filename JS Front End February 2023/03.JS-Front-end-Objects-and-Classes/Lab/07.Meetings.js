92e125204ac4927b703110d2e639bb9ecb04f53b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-09 10:55:08 +0200

    Lab/07.Meetings.js

---
+++ JS Front End February 2023/03.JS-Front-end-Objects-and-Classes/Lab/07.Meetings.js
@@ -0,0 +1,25 @@
+function meetingsParser(inputArr) {
+  let meetings = {};
+
+  for (const line of inputArr) {
+    let [day, name] = line.split(" ");
+    if (!meetings.hasOwnProperty(day)) {
+      meetings[day] = name;
+      console.log(`Scheduled for ${day}`);
+    } else {
+      console.log(`Conflict on ${day}!`);
+    }
+  }
+
+  //   let object = Object.entries(meetings);
+
+  //   for (const [key, value] of object) {
+  //         console.log(`${key} -> ${value}`)
+  //   }
+
+  for (const key in meetings) {
+    console.log(`${key} -> ${meetings[key]}`);
+  }
+}
+
+meetingsParser(["Monday Peter", "Wednesday Bill", "Monday Tim", "Friday Tim"]);
