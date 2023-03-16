25914d8a54a4942b362ebc74ce72fe69677db550
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-16 09:51:05 +0200

    Lab/02. Show-More/

---
+++ JS Front End February 2023/04. JS-Front-End-DOM-and-Events/Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+  const showMore = document.getElementById("more")
+  const span = document.getElementById("text");
+  span.style.display = "inline";
+  showMore.style.display = "none";
+}
