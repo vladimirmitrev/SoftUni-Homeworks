678caad9e724aa348bbe1481e4fd24dd58c59a73
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-18 16:32:36 +0200

    Exercise/05. Fill-Dropdown/

---
+++ JS Front End February 2023/04. JS-Front-End-DOM-and-Events/Exercise/05. Fill-Dropdown/dropdown.js
@@ -0,0 +1,18 @@
+function addItem() {
+  const dropDownMenu = document.getElementById("menu");
+  const newItemTextInput = document.getElementById("newItemText");
+  const newItemValueInput = document.getElementById("newItemValue");
+
+  const newItemText = newItemTextInput.value;
+  const newItemValue = newItemValueInput.value;
+
+  const option = document.createElement("option");
+
+  option.textContent = newItemText;
+  option.value = newItemValue;
+  
+  dropDownMenu.appendChild(option);
+
+  newItemTextInput.value = "";
+  newItemValueInput.value = "";
+}
