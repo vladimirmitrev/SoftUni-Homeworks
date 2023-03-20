8bd8da3a8845d936cd56097115ea23bbd4f0d221
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-20 19:23:03 +0200

    MoreExercise/13. Distance-Converter/

---
+++ JS Front End February 2023/04. JS-Front-End-DOM-and-Events/MoreExercise/13. Distance-Converter/distanceConverter.js
@@ -0,0 +1,27 @@
+function attachEventsListeners() {
+  const [inputUnits, inputDistance] = [
+    document.querySelector("#inputUnits"),
+    document.querySelector("#inputDistance"),
+  ];
+  const [outputUnits, outputDistance] = [
+    document.querySelector("#outputUnits"),
+    document.querySelector("#outputDistance"),
+  ];
+  const convertBtn = document.querySelector("#convert");
+
+  convertBtn.addEventListener("click", () => {
+      const units = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+      };
+      const distance = Number(inputDistance.value) * units[inputUnits.value];
+      outputDistance.disabled = false;
+      outputDistance.value = distance / units[outputUnits.value];
+    });
+}
