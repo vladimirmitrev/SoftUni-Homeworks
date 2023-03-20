a5c726d28467af071853e9ff82830d1b964146d3
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-20 09:56:22 +0200

    MoreExercise/07. Search-in-List/

---
+++ JS Front End February 2023/04. JS-Front-End-DOM-and-Events/MoreExercise/07. Search-in-List/search.js
@@ -0,0 +1,27 @@
+function search() {
+  const searchedTextInput = document.getElementById("searchText");
+  const towns = Array.from(document.querySelectorAll("ul li"));
+  const searchedText = searchedTextInput.value;
+  const result = document.getElementById("result");
+
+  result.textContent = "";
+
+  let matchesFound = 0;
+
+  for (const town of towns) {
+    const searchedTown = town.textContent;
+
+    if (searchedTown.includes(searchedText)) {
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+      matchesFound++;
+    }
+    result.textContent = `${matchesFound} matches found`;
+  }
+  
+  for (const town of towns) {
+   town.style.fontWeight = "";
+   town.style.textDecoration = "";
+ }
+
+}
