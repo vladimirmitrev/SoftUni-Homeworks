21c8658703a9013169777bae079d28dc15b7651f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-03-22 18:23:11 +0200

    05.JS-Front-End-HTTP-and-REST/

---
+++ JS Front End February 2023/05.JS-Front-End-HTTP-and-REST/Lab/04.GitHubRepos/scripts/app.js
@@ -0,0 +1,14 @@
+function loadRepos() {
+  const BASE_URL = "https://api.github.com/users/testnakov/repos";
+
+  const resultContainer = document.getElementById("res");
+
+  fetch(BASE_URL, { method: "GET" })
+    .then((res) => res.text())
+    .then((data) => {
+      resultContainer.textContent = data;
+    })
+    .catch((err) => {
+      resultContainer.textContent = err;
+    });
+}
