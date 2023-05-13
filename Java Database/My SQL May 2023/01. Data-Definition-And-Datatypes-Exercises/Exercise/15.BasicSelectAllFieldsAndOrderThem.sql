b47908d5ecebfdc5749f2ed4ea0ab99f76a20f01
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-13 19:16:20 +0300

    Exercise/15.BasicSelectAllFieldsAndOrderThem.sql

---
+++ Java Database/My SQL May 2023/01. Data-Definition-And-Datatypes-Exercises/Exercise/15.BasicSelectAllFieldsAndOrderThem.sql
@@ -0,0 +1,11 @@
+SELECT * FROM towns ORDER BY `name`;
+SELECT * FROM departments ORDER BY `name`;
+SELECT * FROM employees ORDER BY salary DESC;
+
+
+-- SELECT * FROM towns as t ORDER BY t.`name`;
+-- SELECT * FROM departments as d ORDER BY d.`name`;
+-- SELECT * FROM employees as e ORDER BY e.salary DESC;
+
+
+
