c4514690850f7610216297516642ba7da550bb33
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-13 14:06:38 +0300

    02.Alter02.AlterMinionsTable

---
+++ Java Database/My SQL May 2023/01. Data-Definition-And-Datatypes-Exercises/Exercise/02.AlterMinionsTable.sql
@@ -0,0 +1,10 @@
+alter table towns
+rename column town_id to id;
+
+alter table minions
+add column town_id int;
+
+alter table minions
+add constraint fk_minions_towns
+foreign key minions(town_id)
+references towns(id);
