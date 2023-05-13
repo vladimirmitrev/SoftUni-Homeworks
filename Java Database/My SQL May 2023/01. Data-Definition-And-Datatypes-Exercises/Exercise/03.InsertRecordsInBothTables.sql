c8dfe82db0882b5c195799947c1d49f4f5c34b7d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-13 14:08:48 +0300

    03.InsertRecordsInBothTables

---
+++ Java Database/My SQL May 2023/01. Data-Definition-And-Datatypes-Exercises/Exercise/03.InsertRecordsInBothTables.sql
@@ -0,0 +1,11 @@
+INSERT INTO towns (id, name) values
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+insert into minions(id, name, age, town_id) values
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', null, 2);
+
+select * from minions;
