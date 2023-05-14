47abe2407a50b98748c14675988e684212ea0903
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-14 14:50:43 +0300

    05.SelectEmployeesByMultipleFilters.sql

---
+++ Java Database/My SQL May 2023/02. MySQL-Basic-CRUD/Lab/05.SelectEmployeesByMultipleFilters.sql
@@ -0,0 +1,10 @@
+SELECT *
+ FROM employees
+ WHERE (department_id = 4 AND salary >= 1000)
+ ORDER BY id;
+
+
+
+ -- Write a query to retrieve information about employees,
+ -- who are in department 4 and has a salary higher or equal to 1000.
+ -- Order the information by id.
