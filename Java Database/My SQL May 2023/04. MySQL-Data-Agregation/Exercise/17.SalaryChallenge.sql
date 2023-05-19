ea904f63d65956c7095852a7800b8765f6d82c64
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-19 14:05:13 +0300

    17.SalaryChallenge.sql

---
+++ Java Database/My SQL May 2023/04. MySQL-Data-Agregation/Exercise/17.SalaryChallenge.sql
@@ -0,0 +1,10 @@
+SELECT `first_name`, `last_name`, `department_id`
+FROM `employees` AS current_employee
+WHERE `salary` > (
+			SELECT AVG(`salary`) FROM `employees`AS other_employees
+			WHERE current_employee.`department_id` = other_employees.`department_id`
+)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
+            
+		
