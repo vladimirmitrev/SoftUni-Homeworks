949aa0ac2f1f72856d1ca438463ad355fdb5d499
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-19 14:04:55 +0300

    16.3rdHighestSalary.sql

---
+++ Java Database/My SQL May 2023/04. MySQL-Data-Agregation/Exercise/16.3rdHighestSalary.sql
@@ -0,0 +1,10 @@
+SELECT `department_id`, (
+SELECT DISTINCT `salary` FROM `employees` AS e2
+WHERE e1.`department_id` = e2.`department_id`
+ORDER BY `salary` DESC
+LIMIT 1 OFFSET 2 
+) AS "third_highest_salary"
+FROM `employees` AS e1
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
