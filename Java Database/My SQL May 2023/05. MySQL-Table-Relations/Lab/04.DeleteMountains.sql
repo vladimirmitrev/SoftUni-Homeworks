a9c773b1df7edeb9adcd4e95ff61ad8059a8f166
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-20 14:06:07 +0300

    04.DeleteMountains.sql

---
+++ Java Database/My SQL May 2023/05. MySQL-Table-Relations/Lab/04.DeleteMountains.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `mountains`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL
+);
+CREATE TABLE `peaks`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`mountain_id` INT,
+CONSTRAINT fk_peaks_moutains
+FOREIGN KEY (mountain_id)
+REFERENCES mountains(id)
+ON DELETE CASCADE
+);
