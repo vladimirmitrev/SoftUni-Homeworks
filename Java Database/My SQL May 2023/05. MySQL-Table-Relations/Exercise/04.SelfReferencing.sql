bde67e546c744f53be614f094d020d1ded55431c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-21 16:11:03 +0300

    04.SelfReferencing.sql

---
+++ Java Database/My SQL May 2023/05. MySQL-Table-Relations/Exercise/04.SelfReferencing.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `teachers`(
+`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`manager_id` INT
+);
+
+ALTER TABLE `teachers` AUTO_INCREMENT = 101;
+
+INSERT INTO `teachers` (`name`, `manager_id`)
+VALUES
+	('John', NULL),
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101);
+
+ALTER TABLE `teachers`
+ADD CONSTRAINT fk_managerIdteachers
+FOREIGN KEY (`manager_id`)
+REFERENCES `teachers`(`teacher_id`);
+
+    
