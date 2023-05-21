c8d6f8f9af07936bf9c9a730d00bc5907e24cdb2
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-21 16:13:22 +0300

    07.PeaksInRila.sql

---
+++ Java Database/My SQL May 2023/05. MySQL-Table-Relations/Exercise/07.PeaksInRila.sql
@@ -0,0 +1,8 @@
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` AS 'peak_elevation'
+FROM 
+`mountains` AS m
+JOIN
+`peaks` AS p
+ON m.`id` = p.`mountain_id`
+WHERE mountain_range = "Rila"
+ORDER BY `peak_elevation` DESC;
