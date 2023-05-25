bb4e05474b7968c74eee03b1be50acc1f02af706
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-05-25 19:22:08 +0300

    12.HighestPeaksInBulgaria.sql

---
+++ Java Database/My SQL May 2023/06. MySQL-Joins-Subqueries-and-Indices/Exercise/12.HighestPeaksInBulgaria.sql
@@ -0,0 +1,6 @@
+SELECT mc.`country_code`, m.`mountain_range`, p.`peak_name`, p.`elevation`
+FROM `peaks` AS p
+JOIN `mountains` AS m ON p.`mountain_id` = m.`id`
+JOIN `mountains_countries` as mc ON m.`id` = mc.`mountain_id`
+WHERE mc.`country_code` = "BG" AND p.`elevation` > 2835
+ORDER BY p.`elevation` DESC;
