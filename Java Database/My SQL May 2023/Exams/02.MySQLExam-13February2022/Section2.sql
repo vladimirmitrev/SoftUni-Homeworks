3f7877d5bb6b649fe6578a40aea78cda879fe923
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-02 10:49:35 +0300

    02.MySQLExam-13February2022

---
+++ Java Database/My SQL May 2023/Exams/02.MySQLExam-13February2022/Section2.sql
@@ -0,0 +1,22 @@
+#Section 2: Data Manipulation Language (DML) – 30 pts
+# use online_store
+
+#02.Insert
+INSERT INTO reviews (content, rating, picture_url, published_at)
+SELECT
+    SUBSTRING(p.description, 1, 15),
+    p.price / 8,
+    REVERSE(p.name),
+    (20101010000000)
+    FROM products as p
+WHERE p.id >= 5;
+
+#03.Update
+UPDATE products AS p
+SET p.quantity_in_stock = p.quantity_in_stock - 5
+WHERE p.quantity_in_stock BETWEEN 60 AND 70;
+
+#04.Delete
+DELETE c FROM customers AS c
+LEFT JOIN orders AS o ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
