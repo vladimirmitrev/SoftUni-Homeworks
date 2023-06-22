d27b2f7ccb58807dddbd371a819863ae0fa6df86
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-22 19:51:20 +0300

    04.Hibernate-Code-First-Exercises

---
+++ Java Database/SpringDataDatabase/04.Hibernate-Code-First-Exercises/src/main/java/_01_GringottsDatabase/WizzardDeposits.java
@@ -0,0 +1,176 @@
+package _01_GringottsDatabase;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "_01_wizzard_deposits")
+public class WizzardDeposits {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+    @Column(name = "last_name", length = 60, nullable = false)
+    private String lastName;
+
+    @Column(name = "notes", length = 1000, columnDefinition = "TEXT")
+    private String notes;
+
+    private int age;
+
+    @Column(name = "magic_wand_creator", length = 100)
+    private String magicWandCreator;
+
+    @Column(name = "magic_wand_size")
+    private short magicWandSize;
+
+    @Column(name = "deposit_group", length = 20)
+    private String depositGroup;
+
+    @Column(name = "deposit_start_date", nullable = false)
+    private LocalDateTime depositStartDate;
+
+    @Column(name = "deposit_amount", nullable = false)
+    private BigDecimal depositAmount;
+
+    @Column(name = "deposit_interest", nullable = false)
+    private double depositInterest;
+
+    @Column(name = "deposit_charge", nullable = false)
+    private double depositCharge;
+
+    @Column(name = "deposit_expiration_date", nullable = false)
+    private LocalDateTime depositExpirationDate;
+
+    @Column(name = "is_deposit_expired")
+    private boolean depositExpired;
+
+    public WizzardDeposits(String firstName, String lastName, String notes, int age, String magicWandCreator, short magicWandSize, String depositGroup, LocalDateTime depositStartDate, BigDecimal depositAmount, double depositInterest, double depositCharge, LocalDateTime depositExpirationDate, boolean depositExpired) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.notes = notes;
+        this.age = age;
+        this.magicWandCreator = magicWandCreator;
+        this.magicWandSize = magicWandSize;
+        this.depositGroup = depositGroup;
+        this.depositStartDate = depositStartDate;
+        this.depositAmount = depositAmount;
+        this.depositInterest = depositInterest;
+        this.depositCharge = depositCharge;
+        this.depositExpirationDate = depositExpirationDate;
+        this.depositExpired = depositExpired;
+    }
+
+    public WizzardDeposits() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMagicWandCreator() {
+        return magicWandCreator;
+    }
+
+    public void setMagicWandCreator(String magicWandCreator) {
+        this.magicWandCreator = magicWandCreator;
+    }
+
+    public short getMagicWandSize() {
+        return magicWandSize;
+    }
+
+    public void setMagicWandSize(short magicWandSize) {
+        this.magicWandSize = magicWandSize;
+    }
+
+    public String getDepositGroup() {
+        return depositGroup;
+    }
+
+    public void setDepositGroup(String depositGroup) {
+        this.depositGroup = depositGroup;
+    }
+
+    public LocalDateTime getDepositStartDate() {
+        return depositStartDate;
+    }
+
+    public void setDepositStartDate(LocalDateTime depositStartDate) {
+        this.depositStartDate = depositStartDate;
+    }
+
+    public BigDecimal getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(BigDecimal depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public double getDepositInterest() {
+        return depositInterest;
+    }
+
+    public void setDepositInterest(double depositInterest) {
+        this.depositInterest = depositInterest;
+    }
+
+    public double getDepositCharge() {
+        return depositCharge;
+    }
+
+    public void setDepositCharge(double depositCharge) {
+        this.depositCharge = depositCharge;
+    }
+
+    public LocalDateTime getDepositExpirationDate() {
+        return depositExpirationDate;
+    }
+
+    public void setDepositExpirationDate(LocalDateTime depositExpirationDate) {
+        this.depositExpirationDate = depositExpirationDate;
+    }
+
+    public boolean isDepositExpired() {
+        return depositExpired;
+    }
+
+    public void setDepositExpired(boolean depositExpired) {
+        this.depositExpired = depositExpired;
+    }
+}
+
