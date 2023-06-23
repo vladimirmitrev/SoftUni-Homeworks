0c9202d1bcbbc63ad082b3a0a06d1699ff9774e4
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-23 20:24:44 +0300

    _03_UniversitySystem

---
+++ Java Database/SpringDataDatabase/04.Hibernate-Code-First-Exercises/src/main/java/_03_UniversitySystem/entities/Person.java
@@ -0,0 +1,57 @@
+package _03_UniversitySystem.entities;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    public Person() {}
+
+    public Person(String firstName, String lastName, int phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
