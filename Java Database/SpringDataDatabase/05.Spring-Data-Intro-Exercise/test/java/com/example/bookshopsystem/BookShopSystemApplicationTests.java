4d95f0f7152dacb7fe3f831050c205a0f561fcde
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-24 20:52:23 +0300

    05.Spring-Data-Intro-Exercise

---
+++ Java Database/SpringDataDatabase/05.Spring-Data-Intro-Exercise/test/java/com/example/bookshopsystem/BookShopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookshopsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
