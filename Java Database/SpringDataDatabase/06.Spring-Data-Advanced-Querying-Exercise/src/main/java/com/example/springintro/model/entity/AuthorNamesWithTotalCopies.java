d1ab17cdf439adb4861382287ff3247dad396f93
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-27 21:15:43 +0300

    06.Spring-Data-Advanced-Querying-Exercise

---
+++ Java Database/SpringDataDatabase/06.Spring-Data-Advanced-Querying-Exercise/src/main/java/com/example/springintro/model/entity/AuthorNamesWithTotalCopies.java
@@ -0,0 +1,10 @@
+package com.example.springintro.model.entity;
+
+public interface AuthorNamesWithTotalCopies {
+
+    String getFirstNam();
+
+    String getLastNam();
+
+    long getTotalCopies();
+}
