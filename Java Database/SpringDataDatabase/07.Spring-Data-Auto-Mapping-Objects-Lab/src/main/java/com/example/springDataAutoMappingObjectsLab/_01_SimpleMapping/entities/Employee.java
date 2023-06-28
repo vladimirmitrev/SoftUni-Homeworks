f1bbaad78566ecdf433cf441f720febf7d992aab
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-28 20:57:29 +0300

    07.Spring-Data-Auto-Mapping-Objects-Lab

---
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Lab/src/main/java/com/example/springDataAutoMappingObjectsLab/_01_SimpleMapping/entities/Employee.java
@@ -0,0 +1,64 @@
+package com.example.springDataAutoMappingObjectsLab._01_SimpleMapping.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    private BigDecimal salary;
+
+    private LocalDate birthday;
+
+    private Address address;
+
+
+    public Employee(String firstName, String lastName, BigDecimal salary, LocalDate birthday, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
