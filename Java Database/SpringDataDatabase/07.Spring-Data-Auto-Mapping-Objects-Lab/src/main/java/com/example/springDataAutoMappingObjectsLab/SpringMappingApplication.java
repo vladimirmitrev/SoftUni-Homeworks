ae62546e45e2c6f34cc2a1db2ed4ec44115ee5fe
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-28 21:00:53 +0300

    07.Spring-Data-Auto-Mapping-Objects-Lab

---
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Lab/src/main/java/com/example/springDataAutoMappingObjectsLab/SpringMappingApplication.java
@@ -0,0 +1,12 @@
+package com.example.springDataAutoMappingObjectsLab;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMappingApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringMappingApplication.class, args);
+    }
+
+}
