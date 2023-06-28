33ba77eef859abcd2f2473f012c5347616667346
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-28 20:58:41 +0300

    07.Spring-Data-Auto-Mapping-Objects-Lab

---
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Lab/src/main/java/com/example/springDataAutoMappingObjectsLab/_02_AdvancedMapping/dto/EmployeeDTO.java
@@ -0,0 +1,49 @@
+package com.example.springDataAutoMappingObjectsLab._02_AdvancedMapping.dto;
+
+import java.math.BigDecimal;
+
+public class EmployeeDTO {
+    private String firstName;
+    private String lastName;
+    private BigDecimal salary;
+
+    public EmployeeDTO(String firstName, String lastName, BigDecimal salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public EmployeeDTO() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %.2f",
+                this.getFirstName(), this.getLastName(), this.getSalary());
+    }
+
+}
