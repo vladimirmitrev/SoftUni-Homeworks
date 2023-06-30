8f53637ae13cdd5d87565a00366a4eda35fa03d5
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-06-30 21:13:47 +0300

    07.Spring-Data-Auto-Mapping-Objects-Exercise

---
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/model/entities/User.java
@@ -0,0 +1,97 @@
+package com.example.gamestore.model.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+    @Column(name = "is_admin")
+    private Boolean admin;
+    @ManyToMany
+    private Set<Game> games;
+
+    @OneToMany(targetEntity = Order.class, mappedBy = "buyer")
+    private Set<Order> orders;
+
+
+    public User() {
+        this.games = new HashSet<>();
+        this.orders = new HashSet<>();
+    }
+
+    public User(String email, String password, String fullName) {
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
+}
