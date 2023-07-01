c96c90811f4a5a8675d1504b0118859b54c87041
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-01 20:45:35 +0300

    07.Spring-Data-Auto-Mapping-Objects-Exercise

---
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/repository/GameRepository.java
@@ -0,0 +1,11 @@
+package com.example.gamestore.repository;
+
+import com.example.gamestore.model.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Integer> {
+
+
+}
