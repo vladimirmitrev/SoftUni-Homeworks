b8740481d7200d0e214cfd8ef6275783fb3c9c18
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-02 20:28:48 +0300

    08.JSON-Processing-Exercise\ProductsShop

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/ProductsShop/src/main/java/com/example/productsShop/model/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.productsShop.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public BaseEntity() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
