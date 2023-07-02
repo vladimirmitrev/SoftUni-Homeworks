e3591832deaaf6701c65cac2f795735491dc4f64
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-02 20:28:34 +0300

    08.JSON-Processing-Exercise\ProductsShop

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/ProductsShop/src/main/java/com/example/productsShop/globalConstants/GlobalConstants.java
@@ -0,0 +1,16 @@
+package com.example.productsShop.globalConstants;
+
+public class GlobalConstants {
+
+    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/";
+
+    public static final String OUTPUT_PATH = "src/main/resources/files/out/";
+
+    public static final String PRODUCTS_IN_RANGE_FILE_NAME = "products-in-range.json";
+    public static final String USERS_SOLD_PRODUCTS_FILE_NAME = "users-sold-products.json";
+    public static final String CATEGORIES_BY_PRODUCTS_FILE_NAME = "categories-by-products.json";
+    public static final String USERS_AND_PRODUCTS_FILE_NAME = "users-and-products.json";
+
+
+
+}
