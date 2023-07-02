57ba1a3023095ef930e56e58c78c87d99e19b403
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-02 20:25:23 +0300

    08.JSON-Processing-Exercise\ProductsShop

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/ProductsShop/src/test/java/com/example/productsShop/ProductsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
