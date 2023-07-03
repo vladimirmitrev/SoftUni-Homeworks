aef911e4cd402f43fb906bac517785bdd070786a
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-03 20:35:30 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/model/entity/Customer.java
@@ -0,0 +1,64 @@
+package com.example.cardealer.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+    @Column
+    private String name;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    @Column(name = "is_young_driver")
+    private boolean isYoungDriver;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Sale> sales;
+
+    public Customer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isYoungDriver() {
+        return isYoungDriver;
+    }
+
+    public void setYoungDriver(boolean youngDriver) {
+        isYoungDriver = youngDriver;
+    }
+
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+
+    //    @OneToMany
+//    private List<Car> cars;
+}
