571d4c86336671a437a2c548ef527423b7b97e42
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-03 20:34:34 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/globalConstants/GlobalConstants.java
@@ -0,0 +1,24 @@
+package com.example.cardealer.globalConstants;
+
+public class GlobalConstants {
+
+    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/input/";
+
+    public static final String OUTPUT_PATH = "src/main/resources/files/out/";
+
+    public final static String CARS_FILE_PATH = "cars.json";
+    public final static String CUSTOMERS_FILE_PATH = "customers.json";
+    public final static String PARTS_FILE_PATH = "parts.json";
+    public final static String SUPPLIERS_FILE_PATH = "suppliers.json";
+
+    public static final String ORDERED_CUSTOMERS_FILE_NAME = "ordered-customers.json";
+    public static final String TOYOTA_CARS_FILE_NAME = "toyota-cars.json";
+    public static final String LOCAL_SUPPLIERS_FILE_NAME = "local-suppliers.json";
+    public static final String CARS_AND_PARTS_FILE_NAME = "cars-and-parts.json";
+    public static final String CUSTOMERS_TOTAL_SALES_FILE_NAME = "customers-total-sales.json";
+    public static final String SALES_DISCOUNTS_FILE_NAME = "sales-discounts.json";
+
+
+
+
+}
