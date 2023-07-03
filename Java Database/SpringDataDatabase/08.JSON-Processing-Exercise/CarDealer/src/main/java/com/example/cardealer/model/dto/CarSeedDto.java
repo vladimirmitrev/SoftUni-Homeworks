b374b9719448a96bbf77cddedb8eb2a371e04c40
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-03 20:36:23 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/model/dto/CarSeedDto.java
@@ -0,0 +1,40 @@
+package com.example.cardealer.model.dto;
+
+import com.google.gson.annotations.Expose;
+
+public class CarSeedDto {
+
+    @Expose
+    private String make;
+    @Expose
+    private String model;
+    @Expose
+    private long travelledDistance;
+
+    public CarSeedDto() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public long getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public void setTravelledDistance(long travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+}
