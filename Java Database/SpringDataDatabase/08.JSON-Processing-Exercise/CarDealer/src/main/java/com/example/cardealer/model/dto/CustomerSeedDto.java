9bd4977d7eab2903fa01934b1b96e9e49237273c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-03 20:36:29 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/model/dto/CustomerSeedDto.java
@@ -0,0 +1,44 @@
+package com.example.cardealer.model.dto;
+
+import com.google.gson.annotations.Expose;
+
+import java.time.LocalDateTime;
+
+public class CustomerSeedDto {
+
+    @Expose
+    private String name;
+
+    @Expose
+    private LocalDateTime birthDate;
+
+    @Expose
+    private boolean isYoungDriver;
+
+    public CustomerSeedDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isYoungDriver() {
+        return isYoungDriver;
+    }
+
+    public void setYoungDriver(boolean youngDriver) {
+        isYoungDriver = youngDriver;
+    }
+}
