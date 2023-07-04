f76be08e1fef76512ba2b28b545875d90317278b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-04 17:04:16 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/model/dto/discountSale/CarWithoutPartsDto.java
@@ -0,0 +1,51 @@
+package com.example.cardealer.model.dto.discountSale;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CarWithoutPartsDto {
+
+    @Expose
+    @SerializedName("Make")
+    private String make;
+    @Expose
+    @SerializedName("Model")
+    private String model;
+
+    @Expose
+    @SerializedName("TravelledDistance")
+    private Long travelledDistance;
+
+    public CarWithoutPartsDto() {
+    }
+
+    public CarWithoutPartsDto(String make, String model, Long travelledDistance) {
+        this.make = make;
+        this.model = model;
+        this.travelledDistance = travelledDistance;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Long getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public void setTravelledDistance(Long travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+}
