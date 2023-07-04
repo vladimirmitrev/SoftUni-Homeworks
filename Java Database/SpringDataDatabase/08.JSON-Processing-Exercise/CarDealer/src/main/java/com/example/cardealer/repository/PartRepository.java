b2db676825360235c55134388db188edda4f2621
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-04 17:05:37 +0300

    08.JSON-Processing-Exercise\CarDealer

---
+++ Java Database/SpringDataDatabase/08.JSON-Processing-Exercise/CarDealer/src/main/java/com/example/cardealer/repository/PartRepository.java
@@ -0,0 +1,9 @@
+package com.example.cardealer.repository;
+
+import com.example.cardealer.model.entity.Part;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartRepository extends JpaRepository<Part, Long> {
+}
