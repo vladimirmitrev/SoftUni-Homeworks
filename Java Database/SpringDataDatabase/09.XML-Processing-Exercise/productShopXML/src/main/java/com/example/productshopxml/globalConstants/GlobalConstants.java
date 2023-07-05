5e719a00f76e424727ae0228a503f2d50e4adaf7
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-05 20:49:07 +0300

    09.XML-Processing-Exercise/productShopXML

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/globalConstants/GlobalConstants.java
@@ -0,0 +1,20 @@
+package com.example.productshopxml.globalConstants;
+
+public class GlobalConstants {
+
+    public static final String RESOURCES_FILE_PATH = "src/main/resources/files/";
+
+    public static final String OUTPUT_PATH = "src/main/resources/files/output/";
+
+    public final static String USERS_FILE_PATH = "users.xml";
+    public final static String PRODUCTS_FILE_PATH = "products.xml";
+    public final static String CATEGORIES_FILE_PATH = "categories.xml";
+
+    public static final String PRODUCTS_IN_RANGE_FILE_NAME = "products-in-range.xml";
+    public static final String USERS_SOLD_PRODUCTS_FILE_NAME = "users-sold-products.xml";
+    public static final String CATEGORIES_BY_PRODUCTS_FILE_NAME = "categories-by-products.xml";
+    public static final String USERS_AND_PRODUCTS_FILE_NAME = "users-and-products.xml";
+
+
+
+}
