89424f117bf638dc05b9be6abebc8d23351f913b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-05 20:50:05 +0300

    09.XML-Processing-Exercise/productShopXML

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.productshopxml.repository;
+
+import com.example.productshopxml.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
