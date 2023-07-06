4f2c19024eb60bc8c87619b82734afc992399f90
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-06 18:29:40 +0300

    updates

--- Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/repository/CategoryRepository.java
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/repository/CategoryRepository.java
@@ -1,9 +1,28 @@
 package com.example.productshopxml.repository;
 
+import com.example.productshopxml.model.dto.CategoryWithInfoDto;
 import com.example.productshopxml.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+//    @Query("SELECT c.name, COUNT(p.id), AVG(p.price), SUM(p.price)" +
+//            " FROM Category c" +
+//            " JOIN Product p" +
+//            " GROUP BY c.name" +
+//            " ORDER BY COUNT(p.id)")
+//    List<Category>  getAllCategoriesOrderByProductCount();
+
+    @Query("SELECT new com.example.productshopxml.model.dto.CategoryWithInfoDto(" +
+            "c.name, COUNT(p), AVG(p.price), SUM(p.price))" +
+            " FROM Product p" +
+            " JOIN p.categories c" +
+            " GROUP BY c" +
+            " ORDER BY COUNT(p) DESC")
+    List<CategoryWithInfoDto>getAllCategoriesOrderByProductCount();
 }
