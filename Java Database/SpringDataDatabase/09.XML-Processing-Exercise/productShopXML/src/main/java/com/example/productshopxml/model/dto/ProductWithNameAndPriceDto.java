a469a2ad9c1c8e7ba305cc83f2e86657242d673c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-06 18:27:37 +0300

    updates

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/model/dto/ProductWithNameAndPriceDto.java
@@ -0,0 +1,39 @@
+package com.example.productshopxml.model.dto.seed._4dto;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.math.BigDecimal;
+
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ProductWithNameAndPriceDto {
+    @XmlAttribute(name = "name")
+    private String name;
+
+    @XmlAttribute(name = "price")
+    private BigDecimal price;
+
+
+    public ProductWithNameAndPriceDto() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
