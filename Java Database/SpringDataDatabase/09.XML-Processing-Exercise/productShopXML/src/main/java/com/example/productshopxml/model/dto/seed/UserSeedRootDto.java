b852a21f0a5ed6583af114be08e8aea60459d299
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-06 18:26:48 +0300

    updates

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/model/dto/seed/UserSeedRootDto.java
@@ -0,0 +1,25 @@
+package com.example.productshopxml.model.dto.seed;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.List;
+
+@XmlRootElement(name = "users")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserSeedRootDto {
+
+    @XmlElement(name = "user")
+    private List<UserSeedDto> users;
+
+
+    public List<UserSeedDto> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserSeedDto> users) {
+        this.users = users;
+    }
+}
