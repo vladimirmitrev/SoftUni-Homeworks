92ff82dbc33f0d686a45d82409023d611e79371b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-06 18:25:50 +0300

    updates

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/model/dto/seed/CategorySeedDto.java
@@ -0,0 +1,24 @@
+package com.example.productshopxml.model.dto.seed;
+
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "category")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CategorySeedDto {
+
+    @XmlElement(name = "name")
+    private String name;
+
+    @Size(min = 3, max = 15)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
