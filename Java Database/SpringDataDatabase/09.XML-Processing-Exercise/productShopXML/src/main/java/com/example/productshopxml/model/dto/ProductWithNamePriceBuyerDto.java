993a88dbf271aa6ee65d749f4c2efa38fe750fdc
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-06 18:27:43 +0300

    updates

---
+++ Java Database/SpringDataDatabase/09.XML-Processing-Exercise/productShopXML/src/main/java/com/example/productshopxml/model/dto/ProductWithNamePriceBuyerDto.java
@@ -0,0 +1,61 @@
+package com.example.productshopxml.model.dto;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.math.BigDecimal;
+
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ProductWithNamePriceBuyerDto {
+
+    @XmlElement(name = "name")
+    private String name;
+
+    @XmlElement(name = "price")
+    private BigDecimal price;
+
+    @XmlElement(name = "buyer-first-name")
+    private String buyerFirstName;
+
+    @XmlElement(name = "buyer-last-name")
+    private String buyerLastName;
+
+    public ProductWithNamePriceBuyerDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getBuyerFirstName() {
+        return buyerFirstName;
+    }
+
+    public void setBuyerFirstName(String buyerFirstName) {
+        this.buyerFirstName = buyerFirstName;
+    }
+
+    public String getBuyerLastName() {
+        return buyerLastName;
+    }
+
+    public void setBuyerLastName(String buyerLastName) {
+        this.buyerLastName = buyerLastName;
+    }
+}
+
