1a6480c9fbed82679820b00e00a83d3c190d7c97
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-08 19:39:26 +0300

    Updates

--- Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/repository/GameRepository.java
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/repository/GameRepository.java
@@ -4,8 +4,10 @@ import com.example.gamestore.model.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, Integer> {
 
-
+    List<Game> findGameByTitle(String title);
 }
