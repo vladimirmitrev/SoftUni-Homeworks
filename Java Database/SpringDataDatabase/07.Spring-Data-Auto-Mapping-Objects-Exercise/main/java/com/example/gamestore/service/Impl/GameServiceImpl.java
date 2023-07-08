c6b87b3696bf75ae0759c4b7cf035b231d916e9f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-08 19:40:01 +0300

    Updates

--- Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/service/Impl/GameServiceImpl.java
+++ Java Database/SpringDataDatabase/07.Spring-Data-Auto-Mapping-Objects-Exercise/main/java/com/example/gamestore/service/Impl/GameServiceImpl.java
@@ -1,40 +1,63 @@
-package com.example.gamestore.service;
+package com.example.gamestore.service.Impl;
 
 import com.example.gamestore.model.dto.GameAddDTO;
+import com.example.gamestore.model.dto.OwnedGamesDto;
 import com.example.gamestore.model.entities.Game;
+import com.example.gamestore.model.entities.User;
 import com.example.gamestore.repository.GameRepository;
+import com.example.gamestore.repository.UserRepository;
+import com.example.gamestore.service.GameService;
+import com.example.gamestore.service.UserService;
 import com.example.gamestore.util.ValidationUtil;
 import jakarta.validation.ConstraintViolation;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.Optional;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class GameServiceImpl implements GameService {
 
 
     private final GameRepository gameRepository;
+    private final UserRepository userRepository;
     private final UserService userService;
     private final ModelMapper modelMapper;
     private final ValidationUtil validationUtil;
+    private User loggedInUser;
 
-    public GameServiceImpl(GameRepository gameRepository, UserService userService, ModelMapper modelMapper, ValidationUtil validationUtil) {
+
+    public GameServiceImpl(GameRepository gameRepository, UserRepository userRepository, UserService userService, ModelMapper modelMapper, ValidationUtil validationUtil) {
         this.gameRepository = gameRepository;
+        this.userRepository = userRepository;
         this.userService = userService;
         this.modelMapper = modelMapper;
         this.validationUtil = validationUtil;
+
     }
 
     @Override
     public void addGame(GameAddDTO gameAddDTO) {
 
+
+//        if(loggedInUser == null) {
+//            System.out.println("Please first login!");
+//            return;
+//        }
+
+//        if(!loggedInUser.getAdmin() || loggedInUser.getAdmin() == null) {
+//            System.out.println("Only admins can add,edit or delete games!");
+//            return;
+//        }
+
+
         Set<ConstraintViolation<GameAddDTO>> violations =
                 validationUtil.getViolations(gameAddDTO);
 
-        if(!violations.isEmpty()) {
+        if (!violations.isEmpty()) {
             violations
                     .stream()
                     .map(ConstraintViolation::getMessage)
@@ -59,7 +82,7 @@ public class GameServiceImpl implements GameService {
         Game game = gameRepository.findById(gameId)
                 .orElse(null);
 
-        if(game == null) {
+        if (game == null) {
             System.out.println("Game Id is not correct");
             return;
         }
@@ -75,11 +98,42 @@ public class GameServiceImpl implements GameService {
         Game game = gameRepository.findById(gameId)
                 .orElse(null);
 
-        if(game == null) {
+        if (game == null) {
             System.out.println("Game Id is not correct");
             return;
         }
 
+        System.out.printf("Deleted %s%n", game.getTitle());
         gameRepository.delete(game);
     }
+
+    @Override
+    public void printAllGames() {
+        List<Game> gamesList = gameRepository.findAll();
+
+        gamesList.forEach(game -> System.out.printf("%s %.2f%n", game.getTitle(), game.getPrice()));
+    }
+
+    @Override
+    public void detailsForGame(String currentGame) {
+
+        List<Game> gameByTitle = gameRepository.findGameByTitle(currentGame);
+
+        gameByTitle.forEach(game -> System.out.printf("Title: %s%n" +
+                        "Price: %.2f%n" +
+                        "Description: %s%n" +
+                        "Release date: %s%n", game.getTitle(),
+                game.getPrice(),
+                game.getDescription(), game.getReleaseDate()));
+
+    }
+
+    @Override
+    public void ownedGames() {
+        List<Game> gameList = gameRepository.findAll();
+
+        gameList.forEach(game -> System.out.printf("%s%n", game.getTitle()));
+
+    }
+
 }
