acedb762b77825ef466e2f7ca1a5b65897142735
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-09 15:06:31 +0300

    07.JavaDBSpringDataExam-29Mar2020

---
+++ Java Database/SpringDataDatabase/OldExams/07.JavaDBSpringDataExam-29Mar2020/realDealExam/src/main/java/softuni/exam/service/PictureService.java
@@ -0,0 +1,18 @@
+package softuni.exam.service;
+
+
+import softuni.exam.models.entity.Car;
+
+import java.io.IOException;
+
+//ToDo - Before start App implement this Service and set areImported to return false
+public interface PictureService {
+
+    boolean areImported();
+
+    String readPicturesFromFile() throws IOException;
+	
+	String importPictures() throws IOException;
+
+
+}
