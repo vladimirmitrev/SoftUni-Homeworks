c9bb9e8829db40f8c61d3c21a28e1d1cd2436396
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-09 15:07:11 +0300

    07.JavaDBSpringDataExam-29Mar2020

---
+++ Java Database/SpringDataDatabase/OldExams/07.JavaDBSpringDataExam-29Mar2020/realDealExam/src/main/java/softuni/exam/service/impl/SellerServiceImpl.java
@@ -0,0 +1,83 @@
+package softuni.exam.service.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import softuni.exam.models.entity.Seller;
+import softuni.exam.models.entity.dto.SellerSeedRootDto;
+import softuni.exam.repository.SellerRepository;
+import softuni.exam.service.SellerService;
+import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.XmlParser;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+@Service
+public class SellerServiceImpl implements SellerService {
+
+    private static final String SELLERS_FILE_PATH = "src/main/resources/files/xml/sellers.xml";
+
+
+    private final ModelMapper modelMapper;
+    private final ValidationUtil validationUtil;
+    private final XmlParser xmlParser;
+
+    private final SellerRepository sellerRepository;
+
+    public SellerServiceImpl(ModelMapper modelMapper, ValidationUtil validationUtil, XmlParser xmlParser, SellerRepository sellerRepository) {
+        this.modelMapper = modelMapper;
+        this.validationUtil = validationUtil;
+        this.xmlParser = xmlParser;
+        this.sellerRepository = sellerRepository;
+    }
+
+    @Override
+    public boolean areImported() {
+        return sellerRepository.count() > 0;
+    }
+
+    @Override
+    public String readSellersFromFile() throws IOException {
+
+
+        return Files.readString(Path.of(SELLERS_FILE_PATH));
+    }
+
+    @Override
+    public String importSellers() throws IOException, JAXBException {
+
+        StringBuilder sb = new StringBuilder();
+
+        SellerSeedRootDto sellerSeedRootDto =
+                xmlParser.fromFile(SELLERS_FILE_PATH, SellerSeedRootDto.class);
+
+        sellerSeedRootDto.getSellers()
+                .stream()
+                .filter(sellerSeedDto -> {
+                    boolean isValid = validationUtil.isValid(sellerSeedDto);
+
+                    sb.append(isValid ? String.format("Successfully import seller %s - %s",
+                            sellerSeedDto.getLastName(), sellerSeedDto.getEmail())
+                            : "Invalid seller");
+                    sb.append(System.lineSeparator());
+
+                    return isValid;
+
+                })
+                .map(sellerSeedDto -> modelMapper.map(sellerSeedDto, Seller.class))
+                .forEach(sellerRepository::save);
+
+
+        return sb.toString();
+    }
+
+    @Override
+    public Seller findById(Long id) {
+        return sellerRepository
+                .findById(id)
+                .orElse(null);
+    }
+}
