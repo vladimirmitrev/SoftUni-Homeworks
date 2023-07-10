c0b4945b41bf5b81055f592820099da201e8d490
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-10 13:19:38 +0300

    01.JavaDBSpringDataRetakeExam-17April2022

---
+++ Java Database/SpringDataDatabase/OldExams/01.JavaDBSpringDataRetakeExam-17April2022/softWeatherForecastExam/src/main/java/softuni/exam/models/dto/ForecastSeedDto.java
@@ -0,0 +1,88 @@
+package softuni.exam.models.dto;
+
+import com.google.gson.annotations.Expose;
+import softuni.exam.models.entity.enums.DayOfWeek;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "forecast")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ForecastSeedDto {
+
+    @XmlElement(name = "day_of_week")
+    @NotNull
+    private DayOfWeek dayOfWeek;
+
+    @XmlElement(name = "max_temperature")
+    @Min(-20)
+    @Max(60)
+    private float maxTemperature;
+
+    @XmlElement(name = "min_temperature")
+    @Min(-50)
+    @Max(40)
+    private float minTemperature;
+
+    @XmlElement(name = "sunrise")
+    private String sunrise;
+
+    @XmlElement(name = "sunset")
+    private String sunset;
+
+    @XmlElement(name = "city")
+    private Long city;
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public float getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(float maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public float getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(float minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+
+    public Long getCity() {
+        return city;
+    }
+
+    public void setCity(Long city) {
+        this.city = city;
+    }
+}
