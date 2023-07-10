0ee57e446bc34cf27d8cc91368411ea76a4fe23e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-10 13:19:15 +0300

    01.JavaDBSpringDataRetakeExam-17April2022

---
+++ Java Database/SpringDataDatabase/OldExams/01.JavaDBSpringDataRetakeExam-17April2022/softWeatherForecastExam/src/main/java/softuni/exam/models/dto/CountrySeedDto.java
@@ -0,0 +1,34 @@
+package softuni.exam.models.dto;
+
+import com.google.gson.annotations.Expose;
+
+import javax.validation.constraints.Size;
+
+public class CountrySeedDto {
+
+    @Expose
+    @Size(min = 2, max = 60)
+    private String countryName;
+    @Expose
+    @Size(min = 2, max = 20)
+    private String currency;
+
+    public CountrySeedDto() {
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
