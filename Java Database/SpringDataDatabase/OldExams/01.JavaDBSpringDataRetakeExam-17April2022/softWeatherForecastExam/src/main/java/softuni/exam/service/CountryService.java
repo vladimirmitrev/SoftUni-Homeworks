227825150ea7b6e782c2d65029c196b9d9304fc2
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-10 13:20:19 +0300

    01.JavaDBSpringDataRetakeExam-17April2022

---
+++ Java Database/SpringDataDatabase/OldExams/01.JavaDBSpringDataRetakeExam-17April2022/softWeatherForecastExam/src/main/java/softuni/exam/service/CountryService.java
@@ -0,0 +1,19 @@
+package softuni.exam.service;
+
+
+import softuni.exam.models.entity.Country;
+
+import java.io.IOException;
+import java.util.Optional;
+
+// TODO: Implement all methods
+public interface CountryService {
+
+    boolean areImported();
+
+    String readCountriesFromFile() throws IOException;
+	
+	String importCountries() throws IOException;
+
+    Country findById(Long country);
+}
