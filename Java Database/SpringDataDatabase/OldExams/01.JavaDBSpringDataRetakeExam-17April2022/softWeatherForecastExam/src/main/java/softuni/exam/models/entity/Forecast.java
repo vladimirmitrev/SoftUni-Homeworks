6a68dca8b8d4d1edd09b5b8a24d294acfd28391b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-10 13:18:43 +0300

    01.JavaDBSpringDataRetakeExam-17April2022

---
+++ Java Database/SpringDataDatabase/OldExams/01.JavaDBSpringDataRetakeExam-17April2022/softWeatherForecastExam/src/main/java/softuni/exam/models/entity/Forecast.java
@@ -0,0 +1,84 @@
+package softuni.exam.models.entity;
+
+import softuni.exam.models.entity.enums.DayOfWeek;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "forecasts")
+public class Forecast extends BaseEntity {
+
+    @Column(name = "day_of_week", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @Column(name = "max_temperature", nullable = false)
+    private double maxTemperature;
+
+    @Column(name = "min_temperature", nullable = false)
+    private double minTemperature;
+
+    @Column(name = "sunrise", nullable = false)
+    private LocalTime sunrise;
+
+    @Column(name = "sunset", nullable = false)
+    private LocalTime sunset;
+
+    @ManyToOne
+    private City city;
+
+    public Forecast() {
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public double getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(double maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public double getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(double minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public LocalTime getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(LocalTime sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public LocalTime getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(LocalTime sunset) {
+        this.sunset = sunset;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
