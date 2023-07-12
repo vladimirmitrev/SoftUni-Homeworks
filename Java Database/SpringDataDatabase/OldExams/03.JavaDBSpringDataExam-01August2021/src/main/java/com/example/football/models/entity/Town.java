b7e0ed9f4cec156201a01e429965cd24cc008a62
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-12 13:22:52 +0300

    03.JavaDBSpringDataExam-01August2021

---
+++ Java Database/SpringDataDatabase/OldExams/03.JavaDBSpringDataExam-01August2021/src/main/java/com/example/football/models/entity/Town.java
@@ -0,0 +1,70 @@
+package com.example.football.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "towns")
+public class Town extends BaseEntity {
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "population", nullable = false)
+    private Integer population;
+
+    @Column(name = "travel_guide", nullable = false, columnDefinition = "TEXT")
+    private String travelGuide;
+
+    @OneToMany(mappedBy = "town")
+    private Set<Player> players;
+
+    @OneToMany(mappedBy = "town")
+    private Set<Team> teams;
+
+    public Town() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public String getTravelGuide() {
+        return travelGuide;
+    }
+
+    public void setTravelGuide(String travelGuide) {
+        this.travelGuide = travelGuide;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    public Set<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
+}
