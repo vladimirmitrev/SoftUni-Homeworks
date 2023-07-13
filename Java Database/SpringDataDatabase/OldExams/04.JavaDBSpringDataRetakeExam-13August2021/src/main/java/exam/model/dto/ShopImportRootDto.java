c1d6b2c988330eadc1005b16f88405f8baa5618b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-13 13:20:07 +0300

    04.JavaDBSpringDataRetakeExam-13August2021

---
+++ Java Database/SpringDataDatabase/OldExams/04.JavaDBSpringDataRetakeExam-13August2021/src/main/java/exam/model/dto/ShopImportRootDto.java
@@ -0,0 +1,23 @@
+package exam.model.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "shops")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ShopImportRootDto {
+
+    @XmlElement(name = "shop")
+    private List<ShopImportDto> shops;
+
+    public List<ShopImportDto> getShops() {
+        return shops;
+    }
+
+    public void setShops(List<ShopImportDto> shops) {
+        this.shops = shops;
+    }
+}
