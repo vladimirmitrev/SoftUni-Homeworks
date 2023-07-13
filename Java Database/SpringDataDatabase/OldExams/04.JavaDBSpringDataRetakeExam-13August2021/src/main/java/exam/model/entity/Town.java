fb6556159968b131ed8eb128f62f59e0448c90e6
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-13 13:19:38 +0300

    04.JavaDBSpringDataRetakeExam-13August2021

---
+++ Java Database/SpringDataDatabase/OldExams/04.JavaDBSpringDataRetakeExam-13August2021/src/main/java/exam/model/entity/Town.java
@@ -0,0 +1,70 @@
+package exam.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "towns")
+public class Town extends BaseEntity {
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "population",nullable = false)
+    private int population;
+
+    @Column(name = "travel_guide", nullable = false, columnDefinition = "TEXT")
+    private String travelGuide;
+
+    @OneToMany(mappedBy = "town")
+    private Set<Customer> customers;
+
+    @OneToMany(mappedBy = "town")
+    private Set<Shop> shops;
+
+    public Town() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getTravelGuide() {
+        return travelGuide;
+    }
+
+    public void setTravelGuide(String travelGuide) {
+        this.travelGuide = travelGuide;
+    }
+
+    public Set<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Set<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public Set<Shop> getShops() {
+        return shops;
+    }
+
+    public void setShops(Set<Shop> shops) {
+        this.shops = shops;
+    }
+}
