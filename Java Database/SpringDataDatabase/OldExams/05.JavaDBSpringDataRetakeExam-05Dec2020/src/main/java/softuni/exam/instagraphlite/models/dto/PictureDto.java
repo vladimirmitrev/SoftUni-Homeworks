9c6475c1e9468ec235618e7fdcc4da238d91cb47
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-14 13:10:17 +0300

    05.JavaDBSpringDataRetakeExam-05Dec2020

---
+++ Java Database/SpringDataDatabase/OldExams/05.JavaDBSpringDataRetakeExam-05Dec2020/src/main/java/softuni/exam/instagraphlite/models/dto/PictureDto.java
@@ -0,0 +1,23 @@
+package softuni.exam.instagraphlite.models.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "picture")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PictureDto {
+
+    @XmlElement(name = "path")
+
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
