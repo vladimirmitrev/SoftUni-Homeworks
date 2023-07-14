f7fbe7ef8c49b79e63686ba2c365a1a87682f95c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-14 13:12:53 +0300

    05.JavaDBSpringDataRetakeExam-05Dec2020

---
+++ Java Database/SpringDataDatabase/OldExams/05.JavaDBSpringDataRetakeExam-05Dec2020/src/main/java/softuni/exam/instagraphlite/util/XmlParserImpl.java
@@ -0,0 +1,26 @@
+package softuni.exam.instagraphlite.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@Component
+public class XmlParserImpl implements XmlParser {
+
+    private static final String FORECAST_FILE_PATH = "src/main/resources/files/xml/forecasts.xml";
+
+    private JAXBContext jaxbContext;
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T fromFile(String filePath, Class<T> tClass) throws JAXBException, FileNotFoundException {
+
+        jaxbContext = JAXBContext.newInstance(tClass);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        return (T) unmarshaller.unmarshal(new FileReader(filePath));
+    }
+}
