ce621bebfff224f2f2238a9a6e3530b5986e7c51
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-15 12:50:06 +0300

    06.JavaDBSpringDataRetakeExam-03Apr2020

---
+++ Java Database/SpringDataDatabase/OldExams/06.JavaDBSpringDataRetakeExam-03Apr2020/src/main/java/softuni/exam/models/entity/Ticket.java
@@ -0,0 +1,91 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket extends BaseEntity {
+
+    @Column(name = "serial_number", unique = true)
+    private String serialNumber;
+
+    private BigDecimal price;
+
+    @Column(name = "take_off")
+    private LocalDateTime takeOff;
+
+    @ManyToOne
+    @JoinColumn(name = "from_town_id")
+    private Town fromTown;
+
+    @ManyToOne
+    @JoinColumn(name = "to_town_id")
+    private Town toTown;
+    @ManyToOne
+    private Passenger passenger;
+
+    @ManyToOne
+    private Plane plane;
+
+    public Ticket() {
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getTakeOff() {
+        return takeOff;
+    }
+
+    public void setTakeOff(LocalDateTime takeOff) {
+        this.takeOff = takeOff;
+    }
+
+    public Town getFromTown() {
+        return fromTown;
+    }
+
+    public void setFromTown(Town fromTown) {
+        this.fromTown = fromTown;
+    }
+
+    public Town getToTown() {
+        return toTown;
+    }
+
+    public void setToTown(Town toTown) {
+        this.toTown = toTown;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+}
+
