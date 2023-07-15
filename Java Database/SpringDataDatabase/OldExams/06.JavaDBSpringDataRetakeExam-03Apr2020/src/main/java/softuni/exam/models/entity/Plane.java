02da3aaa73e7eb0be91e2a33a57ce65bc0961fa9
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-15 12:50:00 +0300

    06.JavaDBSpringDataRetakeExam-03Apr2020

---
+++ Java Database/SpringDataDatabase/OldExams/06.JavaDBSpringDataRetakeExam-03Apr2020/src/main/java/softuni/exam/models/entity/Plane.java
@@ -0,0 +1,58 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "planes")
+public class Plane extends BaseEntity {
+
+    @Column(name = "register_number",nullable = false, unique = true)
+    private String registerNumber;
+
+    @Column(nullable = false)
+    private int capacity;
+    @Column(nullable = false)
+    private String airline;
+
+    @OneToMany(mappedBy = "plane")
+    private Set<Ticket> tickets;
+
+    public Plane() {
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public Set<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+}
