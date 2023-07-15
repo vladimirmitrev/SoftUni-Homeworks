71b95c34eaa940013ea8bb50ee7d95be327fc296
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-15 12:51:29 +0300

    06.JavaDBSpringDataRetakeExam-03Apr2020

---
+++ Java Database/SpringDataDatabase/OldExams/06.JavaDBSpringDataRetakeExam-03Apr2020/src/main/java/softuni/exam/models/dto/TicketImportRootDto.java
@@ -0,0 +1,25 @@
+package softuni.exam.models.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "tickets")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TicketImportRootDto {
+    @XmlElement(name = "ticket")
+    private List<TicketImportDto> tickets;
+
+    public TicketImportRootDto() {
+    }
+
+    public List<TicketImportDto> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<TicketImportDto> tickets) {
+        this.tickets = tickets;
+    }
+}
