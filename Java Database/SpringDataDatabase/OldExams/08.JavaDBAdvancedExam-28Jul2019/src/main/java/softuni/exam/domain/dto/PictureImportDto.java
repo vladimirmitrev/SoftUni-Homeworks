b7b61097cd1373675ca6eb21e0618d7651399e26
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-16 11:01:47 +0300

    08.JavaDBAdvancedExam-28Jul2019

---
+++ Java Database/SpringDataDatabase/OldExams/08.JavaDBAdvancedExam-28Jul2019/src/main/java/softuni/exam/domain/dto/PictureImportDto.java
@@ -0,0 +1,23 @@
+package softuni.exam.domain.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "picture")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PictureImportDto {
+
+
+    @NotNull
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
