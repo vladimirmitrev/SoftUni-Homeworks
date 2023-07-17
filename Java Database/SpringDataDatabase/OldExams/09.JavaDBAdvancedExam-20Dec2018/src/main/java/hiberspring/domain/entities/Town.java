fc7a274c4036bc78cf96283ca167f3699974383b
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-17 13:40:25 +0300

    09.JavaDBAdvancedExam-20Dec2018

---
+++ Java Database/SpringDataDatabase/OldExams/09.JavaDBAdvancedExam-20Dec2018/src/main/java/hiberspring/domain/entities/Town.java
@@ -0,0 +1,34 @@
+package hiberspring.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "towns")
+public class Town extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int population;
+
+    public Town() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
