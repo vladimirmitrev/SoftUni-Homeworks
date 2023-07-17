34e522be99f5c8e3b86fd6e22673e106946586ea
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-17 13:41:00 +0300

    09.JavaDBAdvancedExam-20Dec2018

---
+++ Java Database/SpringDataDatabase/OldExams/09.JavaDBAdvancedExam-20Dec2018/src/main/java/hiberspring/domain/dtos/EmployeeImportDto.java
@@ -0,0 +1,76 @@
+package hiberspring.domain.dtos;
+
+import hiberspring.domain.entities.Branch;
+import hiberspring.domain.entities.EmployeeCard;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class EmployeeImportDto {
+
+    @XmlAttribute(name = "first-name")
+    @NotNull
+    private String firstName;
+    @XmlAttribute(name = "last-name")
+    @NotNull
+    private String lastName;
+
+    @XmlAttribute
+    @NotNull
+    private String position;
+
+    @XmlElement
+    @NotNull
+    private String card;
+
+    @XmlElement
+    @NotNull
+    private String branch;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+}
