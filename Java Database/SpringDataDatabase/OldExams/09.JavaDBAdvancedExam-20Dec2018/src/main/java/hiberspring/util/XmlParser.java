ecf5cb14474598760564dad975f5dd3213f6fa52
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-17 13:43:30 +0300

    09.JavaDBAdvancedExam-20Dec2018

---
+++ Java Database/SpringDataDatabase/OldExams/09.JavaDBAdvancedExam-20Dec2018/src/main/java/hiberspring/util/XmlParser.java
@@ -0,0 +1,9 @@
+package hiberspring.util;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+
+public interface XmlParser {
+
+    <T> T fromFile(String filePath, Class<T> tClass) throws JAXBException, FileNotFoundException;
+}
