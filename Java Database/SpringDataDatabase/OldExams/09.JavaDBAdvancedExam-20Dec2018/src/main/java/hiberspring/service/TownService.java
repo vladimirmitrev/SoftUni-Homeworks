d53a5b0e323fa947e757fadc19bf13478c193cd4
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-17 13:42:27 +0300

    09.JavaDBAdvancedExam-20Dec2018

---
+++ Java Database/SpringDataDatabase/OldExams/09.JavaDBAdvancedExam-20Dec2018/src/main/java/hiberspring/service/TownService.java
@@ -0,0 +1,16 @@
+package hiberspring.service;
+
+import hiberspring.domain.entities.Town;
+
+import java.io.IOException;
+
+public interface TownService {
+
+    Boolean townsAreImported();
+
+    String readTownsJsonFile() throws IOException;
+
+    String importTowns(String townsFileContent) throws IOException;
+
+    Town findTownByName(String town);
+}
