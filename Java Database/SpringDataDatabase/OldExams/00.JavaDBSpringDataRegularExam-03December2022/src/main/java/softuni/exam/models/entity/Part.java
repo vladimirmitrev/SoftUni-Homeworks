6cc263a70b41c7b20d2877fad97097498e9e4856
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-18 15:26:39 +0300

    00.JavaDBSpringDataRegularExam-03December2022

---
+++ Java Database/SpringDataDatabase/OldExams/00.JavaDBSpringDataRegularExam-03December2022/src/main/java/softuni/exam/models/entity/Part.java
@@ -0,0 +1,60 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "parts")
+public class Part extends BaseEntity {
+
+    @Column(name = "part_name", nullable = false, unique = true)
+    private String partName;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @OneToMany(mappedBy = "part")
+    private Set<Task> tasks;
+
+    public Part() {
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
+
