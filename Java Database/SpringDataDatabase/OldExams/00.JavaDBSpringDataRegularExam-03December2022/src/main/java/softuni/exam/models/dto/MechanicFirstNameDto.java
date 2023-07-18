e0c9f9965dcd075e5c628684a199d5b719217168
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-18 15:27:33 +0300

    00.JavaDBSpringDataRegularExam-03December2022

---
+++ Java Database/SpringDataDatabase/OldExams/00.JavaDBSpringDataRegularExam-03December2022/src/main/java/softuni/exam/models/dto/MechanicFirstNameDto.java
@@ -0,0 +1,14 @@
+package softuni.exam.models.dto;
+
+public class MechanicFirstNameDto {
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
