06fd6ad347911976fa233bec80c2cd3fb8fd9de7
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-20 12:15:45 +0300

    softWeatherForecastExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/01.JavaDBSpringDataRetakeExam-17April2022/softWeatherForecastExamSecondAttempt/src/main/java/softuni/exam/models/entity/Country.java
@@ -0,0 +1,49 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "countries")
+public class Country extends BaseEntity {
+
+    @Column(name = "country_name", nullable = false, unique = true)
+    private String countryName;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @OneToMany(mappedBy = "country")
+    private Set<City> cities;
+
+    public Country() {
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Set<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(Set<City> cities) {
+        this.cities = cities;
+    }
+}
+
