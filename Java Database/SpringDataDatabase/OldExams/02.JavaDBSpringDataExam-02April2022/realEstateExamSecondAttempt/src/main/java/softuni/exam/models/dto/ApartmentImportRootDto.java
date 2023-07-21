c0f4b6bed884b98eca31c473b5c630ff3d1aa836
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-21 12:42:45 +0300

    realEstateExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/02.JavaDBSpringDataExam-02April2022/realEstateExamSecondAttempt/src/main/java/softuni/exam/models/dto/ApartmentImportRootDto.java
@@ -0,0 +1,23 @@
+package softuni.exam.models.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "apartments")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentImportRootDto {
+
+    @XmlElement(name = "apartment")
+    private List<ApartmentImportDto> apartments;
+
+    public List<ApartmentImportDto> getApartments() {
+        return apartments;
+    }
+
+    public void setApartments(List<ApartmentImportDto> apartments) {
+        this.apartments = apartments;
+    }
+}
