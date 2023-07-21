73f141d54982cfaa22fab5b4fdec4a45004d0cc2
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-21 12:42:40 +0300

    realEstateExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/02.JavaDBSpringDataExam-02April2022/realEstateExamSecondAttempt/src/main/java/softuni/exam/models/dto/ApartmentImportDto.java
@@ -0,0 +1,56 @@
+package softuni.exam.models.dto;
+
+import softuni.exam.models.entity.ApartmentType;
+import softuni.exam.models.entity.Town;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "apartment")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentImportDto {
+
+
+    @NotNull
+    private ApartmentType apartmentType;
+
+    @NotNull
+    @DecimalMin("40.00")
+    private Double area;
+
+    @NotNull
+    private String town;
+
+    public ApartmentType getApartmentType() {
+        return apartmentType;
+    }
+
+    public void setApartmentType(ApartmentType apartmentType) {
+        this.apartmentType = apartmentType;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
