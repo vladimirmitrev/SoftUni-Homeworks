b5beabcec1600c2002c6b1c22fd139e7953bd34f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-23 12:43:38 +0300

    04-02.JavaDBSpringDataRetakeExam-13August2021

---
+++ Java Database/SpringDataDatabase/OldExams/04-02.JavaDBSpringDataRetakeExam-13August2021/src/main/java/exam/model/entities/Shop.java
@@ -0,0 +1,91 @@
+package exam.model.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "shops")
+public class Shop extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal income;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "employee_count", nullable = false)
+    private Integer employeeCount;
+
+    @Column(name = "shop_area", nullable = false)
+    private Integer shopArea;
+
+    @ManyToOne
+    private Town town;
+
+    @OneToMany(mappedBy = "shop")
+    private Set<Laptop> laptops;
+
+
+    public Shop() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getIncome() {
+        return income;
+    }
+
+    public void setIncome(BigDecimal income) {
+        this.income = income;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(Integer employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public Integer getShopArea() {
+        return shopArea;
+    }
+
+    public void setShopArea(Integer shopArea) {
+        this.shopArea = shopArea;
+    }
+
+    public Town getTown() {
+        return town;
+    }
+
+    public void setTown(Town town) {
+        this.town = town;
+    }
+
+    public Set<Laptop> getLaptops() {
+        return laptops;
+    }
+
+    public void setLaptops(Set<Laptop> laptops) {
+        this.laptops = laptops;
+    }
+}
