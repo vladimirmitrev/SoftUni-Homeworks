761544d7fa80faa5b1a3669a4abf5230efe7a3b3
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-23 12:44:03 +0300

    04-02.JavaDBSpringDataRetakeExam-13August2021

---
+++ Java Database/SpringDataDatabase/OldExams/04-02.JavaDBSpringDataRetakeExam-13August2021/src/main/java/exam/model/dtos/ShopDto.java
@@ -0,0 +1,14 @@
+package exam.model.dtos;
+
+public class ShopDto {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
