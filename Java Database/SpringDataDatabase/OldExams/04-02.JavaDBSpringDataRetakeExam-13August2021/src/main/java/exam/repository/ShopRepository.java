b8263531731f69e8200ebe0a3ff27b3e00be4b8e
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-23 12:44:50 +0300

    04-02.JavaDBSpringDataRetakeExam-13August2021

---
+++ Java Database/SpringDataDatabase/OldExams/04-02.JavaDBSpringDataRetakeExam-13August2021/src/main/java/exam/repository/ShopRepository.java
@@ -0,0 +1,12 @@
+package exam.repository;
+
+import exam.model.entities.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+    Optional<Shop> findShopByName(String name);
+}
