01f87f56ea25a15961e1010479999edff779b433
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-24 12:06:14 +0300

    05-02.JavaDBSpringDataRetakeExam-05Dec2020

---
+++ Java Database/SpringDataDatabase/OldExams/05-02.JavaDBSpringDataRetakeExam-05Dec2020/src/main/java/softuni/exam/instagraphlite/models/entities/Picture.java
@@ -0,0 +1,61 @@
+package softuni.exam.instagraphlite.models.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "pictures")
+public class Picture extends BaseEntity {
+
+
+    @Column(nullable = false, unique = true)
+    private String path;
+
+    @Column(nullable = false)
+    private Double size;
+
+    @OneToMany(mappedBy = "picture")
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "picture")
+    private Set<Post> posts;
+
+    public Picture() {
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+}
+
