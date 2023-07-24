9c1c1b2683777dd48ac8e2b7001d5f00332ca00d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-24 12:06:02 +0300

    05-02.JavaDBSpringDataRetakeExam-05Dec2020

---
+++ Java Database/SpringDataDatabase/OldExams/05-02.JavaDBSpringDataRetakeExam-05Dec2020/src/main/java/softuni/exam/instagraphlite/models/entities/User.java
@@ -0,0 +1,58 @@
+package softuni.exam.instagraphlite.models.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Post> posts;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_picture_id")
+    private Picture picture;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
+}
+
