17ed8c5efaa526523ae44b7aa2f70637a5419bca
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-24 12:06:46 +0300

    05-02.JavaDBSpringDataRetakeExam-05Dec2020

---
+++ Java Database/SpringDataDatabase/OldExams/05-02.JavaDBSpringDataRetakeExam-05Dec2020/src/main/java/softuni/exam/instagraphlite/models/dtos/UserImportDto.java
@@ -0,0 +1,47 @@
+package softuni.exam.instagraphlite.models.dtos;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UserImportDto {
+
+    @NotNull
+    @Size(min = 2, max = 18)
+    private String username;
+
+    @NotNull
+    @Size(min = 4)
+    private String password;
+
+
+    @NotNull
+    private String profilePicture;
+
+    public UserImportDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
