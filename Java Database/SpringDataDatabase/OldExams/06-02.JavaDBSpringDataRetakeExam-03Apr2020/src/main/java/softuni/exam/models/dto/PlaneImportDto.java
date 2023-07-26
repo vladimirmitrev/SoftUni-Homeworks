15920d3dcef81d1baef40e10c2faac13c8789715
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-26 10:08:15 +0300

    06-02.JavaDBSpringDataRetakeExam-03Apr2020

---
+++ Java Database/SpringDataDatabase/OldExams/06-02.JavaDBSpringDataRetakeExam-03Apr2020/src/main/java/softuni/exam/models/dto/PlaneImportDto.java
@@ -0,0 +1,54 @@
+package softuni.exam.models.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "plane")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PlaneImportDto {
+
+
+    @XmlElement(name = "register-number")
+    @NotNull
+    @Size(min = 5)
+    private String registerNumber;
+
+    @XmlElement
+    @NotNull
+    @Positive
+    private Integer capacity;
+    @XmlElement
+    @NotNull
+    @Size(min = 2)
+    private String airline;
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+}
