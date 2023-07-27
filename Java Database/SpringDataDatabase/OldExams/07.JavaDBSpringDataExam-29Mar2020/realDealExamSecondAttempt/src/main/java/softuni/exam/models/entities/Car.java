6dcae88e14773a4511862a2d8d0d63683cbcfa06
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-27 12:34:01 +0300

    realDealExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/07.JavaDBSpringDataExam-29Mar2020/realDealExamSecondAttempt/src/main/java/softuni/exam/models/entities/Car.java
@@ -0,0 +1,77 @@
+package softuni.exam.models.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "cars")
+public class Car extends BaseEntity {
+
+    private String make;
+    private String model;
+    private Integer kilometers;
+
+    @Column(name = "registered_on")
+    private LocalDate registeredOn;
+
+    @OneToMany(mappedBy = "car")
+    private Set<Picture> pictures;
+
+    @OneToMany(mappedBy = "car")
+    private Set<Offer> offers;
+
+    public Car() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(Integer kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public LocalDate getRegisteredOn() {
+        return registeredOn;
+    }
+
+    public void setRegisteredOn(LocalDate registeredOn) {
+        this.registeredOn = registeredOn;
+    }
+
+    public Set<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(Set<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+    public Set<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(Set<Offer> offers) {
+        this.offers = offers;
+    }
+}
