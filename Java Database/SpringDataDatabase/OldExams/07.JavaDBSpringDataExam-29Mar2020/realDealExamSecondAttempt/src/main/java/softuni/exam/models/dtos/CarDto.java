f43b572bd6ffd185316273fa1e5da667f4e02774
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-27 12:34:33 +0300

    realDealExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/07.JavaDBSpringDataExam-29Mar2020/realDealExamSecondAttempt/src/main/java/softuni/exam/models/dtos/CarDto.java
@@ -0,0 +1,20 @@
+package softuni.exam.models.dtos;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CarDto {
+
+    @XmlElement
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
