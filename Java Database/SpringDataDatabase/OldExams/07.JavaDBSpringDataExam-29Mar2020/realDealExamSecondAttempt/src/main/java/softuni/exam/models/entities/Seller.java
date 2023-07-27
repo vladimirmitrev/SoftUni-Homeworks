434d405ffa7905ebd0207b764a865a2b9623ac81
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-27 12:34:19 +0300

    realDealExamSecondAttempt

---
+++ Java Database/SpringDataDatabase/OldExams/07.JavaDBSpringDataExam-29Mar2020/realDealExamSecondAttempt/src/main/java/softuni/exam/models/entities/Seller.java
@@ -0,0 +1,76 @@
+package softuni.exam.models.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "sellers")
+public class Seller extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    private Rating rating;
+
+    private String town;
+
+    @OneToMany(mappedBy = "seller")
+    private Set<Offer> offers;
+
+    public Seller() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public Set<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(Set<Offer> offers) {
+        this.offers = offers;
+    }
+}
