b9756beb7412b90eab5b2f84be2bca879863f1e5
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-07-28 12:12:25 +0300

    10.ExamPrepSpringData-May-2023

---
+++ Java Database/SpringDataDatabase/10.ExamPrepSpringData-May-2023/carServiceExam/src/main/java/softuni/exam/models/entity/Task.java
@@ -0,0 +1,72 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task extends BaseEntity {
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+
+    @ManyToOne
+    private Mechanic mechanic;
+
+    @ManyToOne
+    @JoinColumn(name = "cars_id")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "parts_id")
+    private Part part;
+
+    public Task() {
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Mechanic getMechanic() {
+        return mechanic;
+    }
+
+    public void setMechanic(Mechanic mechanic) {
+        this.mechanic = mechanic;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Part getPart() {
+        return part;
+    }
+
+    public void setPart(Part part) {
+        this.part = part;
+    }
+}
