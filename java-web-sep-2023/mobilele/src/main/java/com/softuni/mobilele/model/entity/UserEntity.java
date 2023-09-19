79781c4e01cd883ba7f9a653fddab4b87b477cb0
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-09-19 13:28:54 +0300

    mobilele add entity

---
+++ java-web-sep-2023/mobilele/src/main/java/com/softuni/mobilele/model/entity/UserEntity.java
@@ -0,0 +1,57 @@
+package com.softuni.mobilele.model.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private boolean active;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
