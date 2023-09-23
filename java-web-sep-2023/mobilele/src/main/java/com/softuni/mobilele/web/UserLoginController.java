7b6dac3325cedb989bb73b00562de19c5af3e753
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-09-23 10:09:29 +0300

    mobilele project updates

--- java-web-sep-2023/mobilele/src/main/java/com/softuni/mobilele/web/UserLoginController.java
+++ java-web-sep-2023/mobilele/src/main/java/com/softuni/mobilele/web/UserLoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserLoginControler {
+public class UserLoginController {
 
     @GetMapping("/users/login")
     public String login() {
