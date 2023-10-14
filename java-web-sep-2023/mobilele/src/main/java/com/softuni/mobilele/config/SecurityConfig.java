cb3bb2d01981248ee9374af5448ee427a95ad8a2
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-10-14 17:07:46 +0300

    fixed problem with login using the new spring security

--- java-web-sep-2023/mobilele/src/main/java/com/softuni/mobilele/config/SecurityConfig.java
+++ java-web-sep-2023/mobilele/src/main/java/com/softuni/mobilele/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
@@ -33,91 +35,89 @@ public class SecurityConfig {
     }
 
     // OLD SPRING SECURITY
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+////                .securityMatcher("/") Spring Boot 3.1.0
+//        // define which requests are allowed and which not
+//        http
+//                .authorizeHttpRequests()
+//                // everyone can download static resources (css, js, images)
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//                // everyone can login and register
+//                .requestMatchers("/", "/users/login", "/users/register").permitAll()
+////                .requestMatchers("/users/profile").authenticated()
+////                .requestMatchers("/users/login", "/users/register").anonymous()
+//                // all other pages are available for logger in users
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                // configuration of form login
+//                .formLogin()
+//                // the custom login form
+//                .loginPage("/users/login")
+//                // the name of the username form field
+//                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+//                // the name of the password form field
+//                .passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
+//                // where to go in case that the login is successful
+//                .defaultSuccessUrl("/")
+//                // where to go in case that the login failed
+////                .failureForwardUrl("/users/login-error")
+//                .and()
+//                // configure logut
+//                .logout()
+//                // which is the logout url
+//                .logoutUrl("/users/logout")
+//                .logoutSuccessUrl("/")
+//                // invalidate the session and delete the cookies
+//                .invalidateHttpSession(true)
+////                .clearAuthentication(true)
+//                .deleteCookies("JSESSIONID");
+//
+//
+//        return http.build();
+//    }
+
+    // NEW SPRING SECURITY
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 
 
-//                .securityMatcher("/") Spring Boot 3.1.0
-        // define which requests are allowed and which not
         http
-                .authorizeHttpRequests()
-                // everyone can download static resources (css, js, images)
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                // everyone can login and register
-                .requestMatchers("/", "/users/login", "/users/register").permitAll()
-//                .requestMatchers("/users/profile").authenticated()
-//                .requestMatchers("/users/login", "/users/register").anonymous()
-                // all other pages are available for logger in users
-                .anyRequest()
-                .authenticated()
-                .and()
-                // configuration of form login
-                .formLogin()
-                // the custom login form
-                .loginPage("/users/login")
-                // the name of the username form field
-                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
-                // the name of the password form field
-                .passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
-                // where to go in case that the login is successful
-                .defaultSuccessUrl("/")
-                // where to go in case that the login failed
-//                .failureForwardUrl("/users/login-error")
-                .and()
-                // configure logut
-                .logout()
-                // which is the logout url
-                .logoutUrl("/users/logout")
-//                .logoutSuccessUrl("/")
-                // invalidate the session and delete the cookies
-                .invalidateHttpSession(true)
-//                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID");
+                .authorizeHttpRequests(
+                        authorizeHttpRequests ->
+                                authorizeHttpRequests.
+                                        requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                        .permitAll().
+                                        requestMatchers("/", "/users/login", "/users/register")
+                                        .permitAll().
+//                                        requestMatchers("/").hasRole(UserRoleEnum.USER.name()).
+//                                        requestMatchers("/").hasRole(UserRoleEnum.ADMIN.name()).
+                                        anyRequest().authenticated()
+                )
+                .formLogin(
+                        (formLogin) ->
+                                formLogin.
+                                        loginPage("/users/login").
+                                        usernameParameter(
+                                                UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+                                        passwordParameter(
+                                                UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                                        defaultSuccessUrl("/")
+//                                .failureForwardUrl("/users/login-error")
+                )
+                .logout((logout) ->
+                        logout.logoutUrl("/users/logout").
+                                logoutSuccessUrl("/").//go to homepage after logout
+                                invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                );
 
 
         return http.build();
     }
-
-    // NEW SPRING SECURITY
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//
-//
-//        http
-//                .authorizeHttpRequests(
-//                        authorizeHttpRequests ->
-//                                authorizeHttpRequests.
-//                                        requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-//                                        .permitAll().
-//                                        requestMatchers("/", "/users/login", "/users/register")
-//                                        .permitAll().
-////                                        requestMatchers("/").hasRole(UserRoleEnum.USER.name()).
-////                                        requestMatchers("/").hasRole(UserRoleEnum.ADMIN.name()).
-//                                        anyRequest().authenticated()
-//                )
-//                .formLogin(
-//                        (formLogin) ->
-//                                formLogin.
-//                                        loginPage("/users/login").
-//                                        usernameParameter(
-//                                                UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
-//                                        passwordParameter(
-//                                                UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-//                                        defaultSuccessUrl("/")
-////                                .failureForwardUrl("/users/login-error")
-//                )
-//                .logout((logout) ->
-//                        logout.logoutUrl("/users/logout").
-//                                logoutSuccessUrl("/").//go to homepage after logout
-//                                invalidateHttpSession(true)
-//                                .deleteCookies("JSESSIONID")
-//                );
-//
-//
-//        return http.build();
-//    }
     @Bean public UserDetailsService userDetailsService(UserRepository userRepository) {
         return new MobileleUserDetailsService(userRepository);
     }
@@ -127,6 +127,10 @@ public class SecurityConfig {
                 new RequestAttributeSecurityContextRepository(),
                 new HttpSessionSecurityContextRepository()
         );
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/error", "/resources/**");
     }
 //    @Bean
 //    CorsConfigurationSource corsConfigurationSource() {
