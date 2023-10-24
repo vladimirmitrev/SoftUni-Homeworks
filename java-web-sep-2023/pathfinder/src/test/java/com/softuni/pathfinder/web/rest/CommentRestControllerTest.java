4923b739126899ee80dec2f4b2ed20088a72b43f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2023-10-24 17:19:30 +0300

    added spring security test dependency

--- java-web-sep-2023/pathfinder/src/test/java/com/softuni/pathfinder/web/rest/CommentRestControllerTest.java
+++ java-web-sep-2023/pathfinder/src/test/java/com/softuni/pathfinder/web/rest/CommentRestControllerTest.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -35,7 +37,11 @@ public class CommentRestControllerTest {
     @MockBean
     private CommentService commentService;
 
+//        @MockBean(name = "mockUserDetails")
+//    private UserDetails userDetails;
+
     @Test
+//    @WithUserDetails("mockUserDetails")
     public void getComments_twoCommentsExist_commentsReturnedAsJsonAndStatusIsOk() throws Exception {
 
         when(commentService.getAllCommentsForRoute(ROUTE_ID)).thenReturn(List.of(
