a68a5d6ee4aa4043ae70be2b56d4296450e25ee1
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-10-31 13:25:28 +0200

    Add orderBfs implementation

--- DataStructuresFundamentals2024/DataStructuresTreesRepresentationАndTraversal(BFS-DFS)/src/main/java/implementations/Tree.java
+++ DataStructuresFundamentals2024/DataStructuresTreesRepresentationАndTraversal(BFS-DFS)/src/main/java/implementations/Tree.java
@@ -2,13 +2,44 @@ package implementations;
 
 import interfaces.AbstractTree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Tree<E> implements AbstractTree<E> {
+    private E value;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+    public Tree(E value, Tree<E>... subtrees) {
+        this.value = value;
+        this.parent = null;
+        this.children = new ArrayList<>();
+        for (Tree<E> child : subtrees) {
+            this.children.add(child);
+            child.parent = this;
+        }
+    }
 
     @Override
     public List<E> orderBfs() {
-        return null;
+        List<E> result = new ArrayList<>();
+        Deque<Tree<E>> childrenQueue = new ArrayDeque<>();
+
+        childrenQueue.offer(this);
+
+        while (!childrenQueue.isEmpty()) {
+            Tree<E> current = childrenQueue.poll();
+
+            result.add(current.value);
+
+            for (Tree<E> child : current.children) {
+                childrenQueue.offer(child);
+            }
+        }
+
+        return result;
     }
 
     @Override
@@ -20,8 +51,8 @@ public class Tree<E> implements AbstractTree<E> {
     public void addChild(E parentKey, Tree<E> child) {
 
     }
-	
-	@Override
+
+    @Override
     public void removeNode(E nodeKey) {
 
     }
