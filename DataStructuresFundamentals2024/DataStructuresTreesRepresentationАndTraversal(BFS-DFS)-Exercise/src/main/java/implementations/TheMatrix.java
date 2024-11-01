e472dd6270f024ac879bd96bed057f36dc546e2f
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-11-01 12:54:37 +0200

    Add The Matrix with recursion

--- DataStructuresFundamentals2024/DataStructuresTreesRepresentationАndTraversal(BFS-DFS)-Exercise/src/main/java/implementations/TheMatrix.java
+++ DataStructuresFundamentals2024/DataStructuresTreesRepresentationАndTraversal(BFS-DFS)-Exercise/src/main/java/implementations/TheMatrix.java
@@ -3,7 +3,7 @@ package implementations;
 public class TheMatrix {
     private char[][] matrix;
     private char fillChar;
-    private char toBeReplaced;
+    private char startChar;
     private int startRow;
     private int startCol;
 
@@ -12,15 +12,42 @@ public class TheMatrix {
         this.fillChar = fillChar;
         this.startRow = startRow;
         this.startCol = startCol;
-        this.toBeReplaced = this.matrix[this.startRow][this.startCol];
+        this.startChar = this.matrix[this.startRow][this.startCol];
     }
 
     public void solve() {
-        // TODO: Implement
+        fillMatrix(this.startRow, this.startCol);
+
+    }
+
+    private void fillMatrix(int row, int col) {
+        if (isOutOfBounds(row, col) || this.matrix[row][col] != this.startChar) {
+            return;
+        }
+
+        this.matrix[row][col] = this.fillChar;
+
+//        System.out.println(this.toOutputString());
+//        System.out.println();
+
+        this.fillMatrix(row + 1, col);
+        this.fillMatrix(row, col + 1);
+        this.fillMatrix(row - 1, col);
+        this.fillMatrix(row, col - 1);
+    }
+
+    private boolean isOutOfBounds(int row, int col) {
+        return row < 0 || row >= this.matrix.length || col < 0 || col >= this.matrix[row].length;
     }
 
     public String toOutputString() {
-        // TODO: Implement
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int r = 0; r < this.matrix.length; r++) {
+            for (int c = 0; c < this.matrix[r].length ; c++) {
+                builder.append(this.matrix[r][c]);
+            }
+            builder.append(System.lineSeparator());
+        }
+        return builder.toString().trim();
     }
 }
