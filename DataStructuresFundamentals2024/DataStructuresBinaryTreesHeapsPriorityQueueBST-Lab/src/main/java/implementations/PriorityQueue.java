a2a0d9ce262956d889914a5f38abaff67318e265
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-11-07 19:59:35 +0200

    Add PriorityQueue implementation

--- DataStructuresFundamentals2024/DataStructuresBinaryTreesHeapsPriorityQueueBST-Lab/src/main/java/implementations/PriorityQueue.java
+++ DataStructuresFundamentals2024/DataStructuresBinaryTreesHeapsPriorityQueueBST-Lab/src/main/java/implementations/PriorityQueue.java
@@ -8,23 +8,88 @@ import java.util.List;
 
 public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> {
 
+    private List<E> elements;
+
+    public PriorityQueue() {
+        this.elements = new ArrayList<>();
+    }
     @Override
     public int size() {
-        return 0;
+        return elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
 
+    private void heapifyUp(int index) {
+//        int parentIndex = getParentIndex(index);
+        while (index > 0 && isLess(getParentIndex(index), index)) {
+            Collections.swap(this.elements, getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    private boolean isLess(int first, int second) {
+        return getAt(first).compareTo(getAt(second)) < 0;
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return getAt(0);
+    }
+
+    private void ensureNonEmpty() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("Illegal call to peek on empty heap ");
+        }
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNonEmpty();
+        E returnedValue = getAt(0);
+        Collections.swap(this.elements, 0, this.size() - 1);
+        this.elements.remove(this.size() - 1);
+        this.heapifyDown(0);
+
+        return returnedValue;
+    }
+
+    private void heapifyDown(int index) {
+        while (getLeftChildIndex(index) < this.size() && isLess(index, getLeftChildIndex(index))) {
+            int child = getLeftChildIndex(index);
+            int rightChildIndex = getRightChildIndex(index);
+            if (rightChildIndex < this.size() && isLess(child, rightChildIndex)) {
+                child = rightChildIndex;
+            }
+
+            Collections.swap(this.elements, child, index);
+            index = child;
+        }
+    }
+    private E getLeftChild(int index) {
+        return this.elements.get(this.getLeftChildIndex(index));
+    }
+    private E getRightChild(int index) {
+        return this.elements.get(this.getRightChildIndex(index));
+    }
+    private int getLeftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int getRightChildIndex(int index) {
+        return 2 * index + 2;
     }
 }
