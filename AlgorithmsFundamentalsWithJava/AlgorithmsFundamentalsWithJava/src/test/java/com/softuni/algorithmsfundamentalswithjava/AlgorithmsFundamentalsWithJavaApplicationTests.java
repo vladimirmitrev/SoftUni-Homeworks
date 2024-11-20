a4e0b3ce650acb5ee9302551f7a9a553850ba8fc
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-11-20 18:33:39 +0200

    Initial commit

---
+++ AlgorithmsFundamentalsWithJava/AlgorithmsFundamentalsWithJava/src/test/java/com/softuni/algorithmsfundamentalswithjava/AlgorithmsFundamentalsWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.algorithmsfundamentalswithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsFundamentalsWithJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
