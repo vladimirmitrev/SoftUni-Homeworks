271692ec894b72a996040fb4ef3aa0fc22fe67d5
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-11-21 11:54:11 +0200

    Add P01PermutationsWithoutRepetitions with swap method

--- AlgorithmsFundamentalsWithJava/src/main/java/org/softuni/combinatorialproblems/P01PermutationsWithoutRepetitions.java
+++ AlgorithmsFundamentalsWithJava/src/main/java/org/softuni/combinatorialproblems/P01PermutationsWithoutRepetitions.java
@@ -5,35 +5,45 @@ import java.util.Scanner;
 public class P01PermutationsWithoutRepetitions {
 
     public static String[] elements;
-    public static String[] permutesArr;
-    public static boolean[] used;
+//    public static String[] permutesArr;
+//    public static boolean[] used;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         elements = scanner.nextLine().split("\\s+");
-        permutesArr = new String[elements.length];
-        used = new boolean[elements.length];
 
         permute(0);
+//        permutesArr = new String[elements.length];
+//        used = new boolean[elements.length];
+
     }
 
     private static void permute(int index) {
         if (index == elements.length) {
-            print();
+            print(elements);
             return;
         }
-
-        for (int i = 0; i < elements.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                permutesArr[index] = elements[i];
-                permute(index + 1);
-                used[i] = false;
-            }
+        permute(index + 1);
+        for (int i = index + 1; i < elements.length; i++) {
+            swap(elements, index, i);
+            permute(index + 1);
+            swap(elements, index, i);
+//            if (!used[i]) {
+//                used[i] = true;
+//                permutesArr[index] = elements[i];
+//                permute(index + 1);
+//                used[i] = false;
+//            }
         }
     }
 
-    private static void print() {
-        System.out.println(String.join(" ", permutesArr));
+    private static void swap(String[] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
     }
 }
